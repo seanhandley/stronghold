9264bf795cdfc0a9b72b6e08e0efb8f16b4a685c
Sean Handley <sean.handley@gmail.com>
2014-05-21 15:10:31 +0100

    Modify should include read implicitly.

--- app/models/user/ability.rb
+++ app/models/user/ability.rb
@@ -2,14 +2,14 @@ class User::Ability
   include CanCan::Ability
 
   def initialize(user)
-    alias_action :new, :create, :edit, :update, :destroy, :to => :modify
+    alias_action :new, :create, :edit, :update, :destroy, :read, :to => :modify
 
     can :read, Instance if user.has_permission?('instances.read')
     can :read, Role if user.has_permission?('roles.read')
-    can [:read, :modify], Role if user.has_permission?('roles.read')
+    can :modify, Role if user.has_permission?('roles.modify')
 
     ## Power User can do everything
-    can [:read, :modify], :all if user.power_user?
+    can :modify, :all if user.power_user?
 
   end
 end
