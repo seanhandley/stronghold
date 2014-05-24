17ce88fab25d2e78d32d4b14f08e175e254fe253
Sean Handley <sean.handley@gmail.com>
2014-05-24 22:50:37 +0100

    Throw in more OpenStack object types.

--- lib/open_stack_object.rb
+++ lib/open_stack_object.rb
@@ -55,9 +55,39 @@ module OpenStackObject
 
   end
 
-  class Compute < Base
+  class Server < Base
     def self.collection_name ; :servers ; end
     def self.object_name     ; :compute ; end
+  end
+
+  class Tenant < Base
+    def self.collection_name ; :tenants  ; end
+    def self.object_name     ; :identity ; end
+  end
+
+  class User < Base
+    def self.collection_name ; :users    ; end
+    def self.object_name     ; :identity ; end
+  end
+
+  class Volume < Base
+    def self.collection_name ; :volumes  ; end
+    def self.object_name     ; :volume   ; end   
+  end
+
+  class Image < Base
+    def self.collection_name ; :images  ; end
+    def self.object_name     ; :image   ; end   
+  end
+
+  class Subnet < Base
+    def self.collection_name ; :subnets ; end
+    def self.object_name     ; :network ; end
+  end
+
+  class Resource < Base
+    def self.collection_name ; :resources ; end
+    def self.object_name     ; :metering ; end
   end
 
 end
