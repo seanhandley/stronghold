62577ee4fbb515740d10fae74efe5f376c30cb00
Sean Handley <sean.handley@gmail.com>
2014-05-25 00:39:23 +0100

    Use the Compute provider.

--- lib/open_stack_object.rb
+++ lib/open_stack_object.rb
@@ -72,7 +72,7 @@ module OpenStackObject
 
   class Volume < Base
     def self.collection_name ; :volumes  ; end
-    def self.object_name     ; :volume   ; end   
+    def self.object_name     ; :compute  ; end   
   end
 
   class Image < Base
