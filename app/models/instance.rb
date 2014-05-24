9e19a8707a639e865106626f3bcc8593e4258661
Sean Handley <sean.handley@gmail.com>
2014-05-24 20:51:00 +0100

    Extract the clever stuff into a common base.

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -1,54 +1,7 @@
 require 'fog'
 
-class Instance
+class Instance < OpenStackObject::Compute
 
-  def initialize(server)
-    @server = server
-    delegations.each{|a| class_eval{|c| delegate a.to_sym, to: :server}}
-  end
-
-  def to_s
-    "#<Instance:#{id} #{delegations.collect{|d| "@#{d}=#{send(d.to_sym).inspect}"}.join ', '}>"
-  end
-
-  def id
-    server.id
-  end
-
-  def inspect
-    to_s
-  end
-
-  class << self
-
-    def all
-      compute.servers.map{|s| new(s) }
-    end
-
-    def find_all_by(attribute, value)
-      [compute.servers.find {|s| s.send(attribute.to_sym) == value}].flatten.compact.map do |s|
-        new(s)
-      end
-    end
-    
-    private
-
-    def compute
-      @@c ||= Fog::Compute.new(OPENSTACK_ARGS)
-    end
-
-  end
-
-  private
-
-  # Items to delegate down into the server object
-  # Add as needed
-  def delegations
-    %w{name state}
-  end
-
-  def server
-    @server
-  end
+  attributes :name, :state
 
 end
---
+++ app/models/open_stack_object.rb
@@ -0,0 +1,57 @@
+require 'fog'
+
+module OpenStackObject
+
+  class Base
+    def initialize(obj)
+      @obj = obj
+    end
+
+    def to_s
+      "#<#{self.class.to_s}:#{id} #{@@attributes.collect{|d| "@#{d}=#{send(d.to_sym).inspect}"}.join ', '}>"
+    end
+
+    def id
+      obj.id
+    end
+
+    def inspect
+      to_s
+    end
+
+    def self.attributes(*args)
+      @@attributes = args.each{|a| class_eval{|c| delegate a.to_sym, to: :obj}}
+    end
+
+    def obj
+      @obj
+    end
+
+    class << self
+
+      def all
+        conn.send(collection_name).map{|s| new(s) }
+      end
+
+      def find_all_by(attribute, value)
+        [conn.send(collection_name).find {|s| s.send(attribute.to_sym) == value}].flatten.compact.map do |s|
+          new(s)
+        end
+      end
+
+      private
+
+      def conn
+        @@c ||= "Fog::#{object_name.to_s.titleize}".constantize.new(OPENSTACK_ARGS)
+      end
+
+    end
+
+  end
+
+  class Compute < Base
+    def self.collection_name ; :servers ; end
+    def self.object_name     ; :compute ; end
+  end
+
+end
