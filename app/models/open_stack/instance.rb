42c8cfe086212cdf55fd9365560af3bc95cd1436
Sean Handley <sean.handley@gmail.com>
2014-05-27 15:42:27 +0100

    Split out into files, add power cycle methods.

--- app/models/open_stack/instance.rb
+++ app/models/open_stack/instance.rb
@@ -5,5 +5,38 @@ module OpenStack
 
     methods :reboot, :wait_for
 
+    def initialize(params)
+      super(params)
+      wait_for { ready? } if state == nil
+    end
+
+    def start
+      service_method do |s|
+        if state.downcase == 'paused'
+          s.unpause_server(id)
+        else
+          s.resume_server(id)
+        end
+        wait_for { ready? }
+        reload
+      end
+    end
+
+    def stop
+      service_method do |s|
+        s.suspend_server(id)
+        wait_for { state.downcase == 'suspended' }
+        reload
+      end
+    end
+
+    def pause
+      service_method do |s|
+        s.pause_server(id)
+        wait_for { state.downcase == 'paused' }
+        reload
+      end
+    end
+
   end
 end
