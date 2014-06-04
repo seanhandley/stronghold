2a93a617b8a83e61b518e0ee236ff9136386fe34
Sean Handley <sean.handley@gmail.com>
2014-06-04 12:53:06 +0100

    Return proper http code.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,7 +2,12 @@ class ErrorsController < ApplicationController
 
   layout 'error'
 
-  def page_not_found ; end
-  def server_error ; end
+  def page_not_found
+    render :status => 404, :formats => [:html]
+  end
+
+  def server_error
+    render :status => 500, :formats => [:html]
+  end
 
 end
