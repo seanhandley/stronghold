4cae1c754e682b0a72acc1f4f80d102481410fa1
James Garner <james@jadaradix.com>
2014-06-10 12:02:22 +0100

    Display support ticket summary/description.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140609124454) do
+ActiveRecord::Schema.define(version: 20140610095433) do
 
   create_table "foos", force: true do |t|
     t.string   "name"
