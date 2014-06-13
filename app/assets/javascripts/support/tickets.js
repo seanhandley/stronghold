a2c9076bf95cd275de185c8b480fa0d00271e2e3
James Garner <james@jadaradix.com>
2014-06-13 16:22:16 +0100

    Support tickets loaded via AJAX, but jQuery. Cross the Angular bridge when we come to it. This solution is still lovely.

--- app/assets/javascripts/support/tickets.js
+++ app/assets/javascripts/support/tickets.js
@@ -1,9 +1,22 @@
-jQuery.extend(window, Routes);
-$.ajax({
-  url: Routes.support_api_tickets_path(),
-  cache: false
-}).done(
-  function(html) {
-    console.log(html);
-  }
-);
+$(window).load(function () {
+
+  $.extend(window, Routes);
+
+  $.ajax({
+    url: Routes.support_api_tickets_path(),
+    cache: false
+  }).done(
+    function(json) {
+      var tickets = [];
+      console.log(json);
+      $.each(json, function (id, jsonElement) {
+        attrs = jsonElement.attrs;
+        key = attrs.key;
+        fields = attrs.fields;
+        tickets.push('<li>' + key + '</li>');
+      });
+      $("#tester").html(tickets.join(''));
+    }
+  );
+
+});
