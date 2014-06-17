99ea802f9077653add87fc6daa6cafff067e718b
James Garner <james@jadaradix.com>
2014-06-17 17:13:36 +0100

    Angular progress!

--- app/assets/javascripts/angular/controllers/tickets_ctrl.js.coffee
+++
@@ -1,4 +0,0 @@
-stronghold.controller 'TicketsCtrl', ['$scope', 'Ticket', ($scope, Ticket) ->
-  $scope.tickets_open = Ticket.query()
-  $scope.tickets_closed = Ticket.query()
-]
--- app/models/organization_tickets.rb
+++ app/models/organization_tickets.rb
@@ -1,11 +1,5 @@
 class OrganizationTickets
 
-  module IssueStatus
-    ToDo       = 'To Do'
-    InProgress = 'In Progress'
-    Done       = 'Done'
-  end
-
   def initialize(reference)
     @reference = reference
   end
@@ -13,22 +7,6 @@ class OrganizationTickets
   def all
     Rails.cache.fetch("organization_#{@reference}_tickets", expires_in: 20.seconds) do
       @tickets = SupportTicket.all @reference
-    end
-  end
-
-  def open
-    all.select do |issue|
-      [
-        ([IssueStatus::ToDo, IssueStatus::InProgress].include?(issue.fields['status']['name'])),
-      ].all?
-    end
-  end
-
-  def closed
-    all.select do |issue|
-      [
-        (issue.fields['status']['name'] == IssueStatus::Done)
-      ].all?
     end
   end
 
