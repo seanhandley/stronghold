73d88e5836f30402ee0d4f6c89c8929ecead5ef8
Sean Handley <sean.handley@gmail.com>
2014-06-18 16:17:08 +0100

    Temp.

--- app/helpers/roles_helper.rb
+++ app/helpers/roles_helper.rb
@@ -2,4 +2,8 @@ module RolesHelper
   def list_of_roles(user)
     user.roles.map(&:name).join(', ')
   end
+
+  def roles_for_select(organization)
+    options_for_select(organization.roles.collect{|r| [r.name, r.id]})
+  end
 end
