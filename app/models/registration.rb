8a6190be9669239f0aa0df5579510b738f2dea56
Sean Handley <sean.handley@gmail.com>
2014-06-23 21:04:50 +0100

    Registration tests.

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -11,26 +11,27 @@ class Registration
     @organization_name = params[:organization_name]
   end
 
-  def valid?
+  def process!
     if !invite.can_register?
       errors.add :base, 'Signup token is not valid'
     elsif password != confirm_password
       errors.add :base,  'Passwords do not match'
     elsif password.length < 8
       errors.add :base,  'Password is too short'
-    elsif !invite.organization && organization_name.blank?
+    elsif invite.power_invite? && organization_name.blank?
       errors.add :base, 'Organization name is blank'
     else
-      if invite.organization
-        @organization = invite.organization
-      else
+      if invite.power_invite?
         @organization = Organization.create(name: organization_name)
         @owners = @organization.roles.create name: 'Owners', power_user: true
+      else
+        @organization = invite.organization
       end
       
       roles = (invite.roles + [@owners]).flatten.compact
       @user = @organization.users.create email: invite.email, password: password,
                                          roles: roles
+      @invite.complete!
       return true
     end 
     false
