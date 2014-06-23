477733762469d5a194e5ca77b4da26cf2d682cea
Sean Handley <sean.handley@gmail.com>
2014-06-23 09:39:55 +0100

    Use the AJAX JavaScript error rendering.

--- app/controllers/support/invites_controller.rb
+++ app/controllers/support/invites_controller.rb
@@ -3,23 +3,25 @@ class Support::InvitesController < SupportBaseController
   load_and_authorize_resource param_method: :create_params
 
   def create
-    roles = create_params[:roles].collect do |r|
-      current_user.organization.roles.find(r.to_i)
+    roles = [create_params[:role_ids]].flatten.compact.collect do |r|
+      current_user.organization.roles.find_by_id(r.to_i)
     end.compact
 
-    @invite = current_user.organization.create(create_params)
+    @invite = current_user.organization.invites.create(create_params)
     if @invite.save
       MailWorker.perform_async(:signup, @invite.id)
       javascript_redirect_to support_roles_path
     else
-      render text: 'herp'
+      respond_to do |format|
+        format.js { render :template => "shared/dialog_errors", :locals => {:object => @invite} }
+      end
     end
   end
 
   private
 
   def create_params
-    params.require(:invite).permit(:email, :roles => [])
+    params.require(:invite).permit(:email, :role_ids => [])
   end
 
 end
