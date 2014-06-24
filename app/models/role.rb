79c09d889c56f6cac843ed79f18147801f3f179c
Sean Handley <sean.handley@gmail.com>
2014-06-24 11:28:20 +0100

    Allow deletion of roles.

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,7 @@
 class Role < ActiveRecord::Base
   has_and_belongs_to_many :users
   belongs_to :organization
+  before_destroy :check_users, :check_power
 
   serialize :permissions
 
@@ -12,5 +13,21 @@ class Role < ActiveRecord::Base
 
   def has_permission?(permission)
     power_user || permissions.include?(permission)
+  end
+
+  private
+
+  def check_users
+    if users.present?
+      errors.add(:base, 'You cannot remove a role which has users assigned to it')
+      false
+    end
+  end
+
+  def check_power
+    if power_user?
+      errors.add(:base, 'This is a power user group and may not be removed')
+      false
+    end  
   end
 end
