b4acd3b2f0fab20706e58683d0b88ddb8d88b4bc
Sean Handley <sean.handley@gmail.com>
2014-06-26 17:28:32 +0100

    Use the new ORM for the roles_users table.

--- app/controllers/support/role_users_controller.rb
+++ app/controllers/support/role_users_controller.rb
@@ -2,23 +2,20 @@ class Support::RoleUsersController < SupportBaseController
 
   skip_authorization_check
 
-  before_filter :get_user, :get_role
+  before_filter :get_role_user
 
   def destroy
-    if @user.roles.delete(@role)
+    if @role_user.destroy
       redirect_to support_roles_path(tab: 'roles')
     else
-      redirect_to support_roles_path(tab: 'roles'), notice: "Couldn't remove."
+      redirect_to support_roles_path(tab: 'roles'), notice: @role_user.errors.full_messages.join
     end
   end
 
   private
 
-  def get_user
-    @user = User.find(params[:user_id]) if params[:user_id]
-  end
-
-  def get_role
-    @role = Role.find(params[:role_id]) if params[:role_id]
+  def get_role_user
+    @role_user = RoleUser.where(role_id: params[:role_id], user_id: params[:user_id]).first
+    @role_user.current_user = current_user
   end
 end
