d519e4d2251b82d5b0e8c33e0cd23093415b0dcf
Sean Handley <sean.handley@gmail.com>
2014-08-07 21:15:40 +0100

    Reflect user changes in the OS tenant.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ActiveRecord::Base
   validates :email, :presence => true
   validate :password_complexity
 
+  before_save :set_os_user
+
   def has_permission?(permission)
     power_user? || roles.collect(&:permissions).flatten.include?(permission)
   end
@@ -29,4 +31,20 @@ class User < ActiveRecord::Base
       errors.add(:base,  'Password is too short')
     end
   end
+
+  def set_os_user
+    params =  { email: email, name: "#{organization.reference}_#{email}",
+                tenant_id: organization.tenant_id,
+                enabled: true
+              }
+    params[:password] = password if password.present?
+    if new_record?
+      u = OpenStack::User.create params
+      update_column(:openstack_id, u.id)
+    else
+      u = OpenStack::User.find_all_by(:id, openstack_id)[0]
+      u.update params
+    end
+  end
+
 end
