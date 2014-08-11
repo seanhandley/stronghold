7b2b3f3cb1a62a75fefbe943d5ae3bcf3f0dba33
James Garner <james@jadaradix.com>
2014-08-11 12:49:31 +0100

    Boilerplate for creating a ticket.

--- app/controllers/support/api/tickets_controller.rb
+++ app/controllers/support/api/tickets_controller.rb
@@ -1,22 +1,25 @@
 class Support::Api::TicketsController < SupportBaseController
 
   skip_authorization_check
+  skip_before_action :verify_authenticity_token
 
   def index
     respond_to do |format|
       format.json {
-        render :json => current_user.organization.tickets
+        render :json => current_user.organization.tickets.all
       }
       format.html
     end
   end
 
-  def x
+  def create
+    reference = current_user.organization.tickets.create(params[:title], params[:description])
     respond_to do |format|
       format.json {
-        render :json => "xyzxyz"
+        render :json => {
+          "reference" => reference
+        }
       }
-      format.html
     end
   end
 
