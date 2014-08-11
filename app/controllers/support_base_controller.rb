fce4e034be24819678c171fdde4ca63016990dfd
Sean Handley <sean.handley@gmail.com>
2014-08-11 13:52:03 +0100

    Set the locale on each controller action.

--- app/controllers/support_base_controller.rb
+++ app/controllers/support_base_controller.rb
@@ -3,6 +3,7 @@ class SupportBaseController < ApplicationController
   before_filter :current_user, :authenticate_user!
   before_filter { Authorization.current_user = current_user }
   around_filter :user_time_zone, :if => :current_user
+  before_action :set_locale
 
   helper_method :current_section
 
@@ -16,11 +17,15 @@ class SupportBaseController < ApplicationController
     redirect_to support_root_url, :alert => exception.message
   end
 
+  private
+
+  def set_locale
+    I18n.locale = current_user.present? ? current_user.organization.locale.to_sym : I18n.default_locale
+  end
+
   def user_time_zone(&block)
     Time.use_zone(current_user.organization.time_zone, &block)
   end
-
-  private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
