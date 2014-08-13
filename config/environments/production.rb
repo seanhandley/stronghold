aac42b84c9a43f8c423e557c18f3c021181997cd
Sean Handley <sean.handley@gmail.com>
2014-08-13 12:37:53 +0100

    Revert "Dir path."
    This reverts commit 38296dbda51ca23480106695dd632a245e991d01.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   config.log_level = :info
   config.logger = ActiveSupport::Logger.new("/var/log/rails/stronghold/production.log")
   config.logstasher.enabled = true
-  config.logstasher.logger_path = "/var/log/rails/stronghold/"
+  config.logstasher.logger_path = "/var/log/rails/stronghold/logstash_production.log"
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
