c6fb6281b5c6e4c3dc5a3b3a9cf5e85b19413a54
James Garner <james@jadaradix.com>
2014-08-19 09:52:23 +0100

    Disable Rails caching. Fix new ticket functionality <directives must be in a controller>.

--- lib/tickets.rb
+++ lib/tickets.rb
@@ -12,14 +12,12 @@ class Tickets
   end
 
   def all
-    Rails.cache.fetch("organization_#{@reference}_tickets", expires_in: 20.seconds) do
-      url = @settings['base_url'] + 'search/?jql=project=' + @settings['project'] + ' AND labels in ("' + @reference + '")&fields=*all'
-      response = @connection.get url
-      responseBody = JSON.parse response.body
-      responseBody['issues'].collect do |jira_issue|
-        Ticket.new(jira_issue)
-        # jira_issue
-      end
+    url = @settings['base_url'] + 'search/?jql=project=' + @settings['project'] + ' AND labels in ("' + @reference + '")&fields=*all'
+    response = @connection.get url
+    responseBody = JSON.parse response.body
+    responseBody['issues'].collect do |jira_issue|
+      Ticket.new(jira_issue)
+      # jira_issue
     end
   end
 
