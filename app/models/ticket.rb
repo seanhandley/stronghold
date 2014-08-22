2c1c62b561643fb2c7a794046271662d2d6762a5
James Garner <james@jadaradix.com>
2014-08-22 16:49:20 +0100

    Show the created and updated times from Jira.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -6,6 +6,8 @@ class Ticket
     @title = jira_issue['fields']['summary']
     @description = markdown jira_issue['fields']['description']
     @jira_status = jira_issue['fields']['status']['name']
+    @time_created = jira_issue['fields']['created']
+    @time_updated = jira_issue['fields']['updated']
     @comments = jira_issue['fields']['comment']['comments'].collect do |jira_comment|
       # jira_comment
       TicketComment.new(jira_comment)
