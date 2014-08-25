768b45eafc1fc188027649e768a552c880149236
Sean Handley <sean.handley@gmail.com>
2014-08-25 15:30:55 +0100

    Test honey badger notifications.

--- config/routes.rb
+++ config/routes.rb
@@ -30,9 +30,10 @@ Rails.application.routes.draw do
   root :to => 'support/dashboard#index'
 
   if Rails.env.production?
-    get '404', :to => 'errors#page_not_found'
-    get '422', :to => 'errors#server_error'
-    get '500', :to => 'errors#server_error'
+    get '404',  :to => 'errors#page_not_found'
+    get '422',  :to => 'errors#server_error'
+    get '500',  :to => 'errors#server_error'
+    get 'boom', :to => 'errors#boom'
   end
 
 end
