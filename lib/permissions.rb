deba7dcea31c7fca460b78e1db3c1b50ef0f25c3
Sean Handley <sean.handley@gmail.com>
2014-08-26 17:10:40 +0100

    Not currently in use.

--- lib/permissions.rb
+++ lib/permissions.rb
@@ -3,8 +3,8 @@ class Permissions
     def user
       {
         # Instances
-        'instances.read' => { :description => I18n.t(:can_instances_read), :group => I18n.t(:instances) },
-        'instances.modify' => { :description => I18n.t(:can_instances_modify), :group => I18n.t(:instances) },
+        # 'instances.read' => { :description => I18n.t(:can_instances_read), :group => I18n.t(:instances) },
+        # 'instances.modify' => { :description => I18n.t(:can_instances_modify), :group => I18n.t(:instances) },
 
         # Roles
         'roles.read' => { :description => I18n.t(:can_roles_read), :group => I18n.t(:roles) },
