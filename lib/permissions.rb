32fb03f0e3fc23b28930e18128c8320340084333
Sean Handley <sean.handley@gmail.com>
2014-08-26 17:48:44 +0100

    Add permissions for support tickets.

--- lib/permissions.rb
+++ lib/permissions.rb
@@ -9,7 +9,11 @@ class Permissions
         # Roles
         'roles.read' => { :description => I18n.t(:can_roles_read), :group => I18n.t(:roles) },
         'roles.modify' => { :description => I18n.t(:can_roles_modify), :group => I18n.t(:roles) },
-        'roles.invite' => { :description => I18n.t(:can_roles_invite), :group => I18n.t(:roles) }
+        'roles.invite' => { :description => I18n.t(:can_roles_invite), :group => I18n.t(:roles) },
+
+        # Support Tickets
+        'tickets.read' => { :description => I18n.t(:can_tickets_read), :group => I18n.t(:tickets) },
+        'tickets.modify' => { :description => I18n.t(:can_tickets_modify), :group => I18n.t(:tickets) },
 
       }
     end
