d84f53b8264c5d570611675a67068ace9a1f8ead
Sean Handley <sean.handley@gmail.com>
2014-08-26 10:08:14 +0100

    Unicorn herder will take care of this for us.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -19,9 +19,6 @@ listen "/home/rails/stronghold/tmp/.unicorn.sock", :backlog => 64
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
 
-# feel free to point this anywhere accessible on the filesystem
-pid "/home/rails/stronghold/tmp/unicorn.pid"
-
 # combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
 preload_app true
