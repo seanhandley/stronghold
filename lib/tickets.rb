6cb3ad4186133f32db7e8196575b07ad7948309b
Sean Handley <sean.handley@gmail.com>
2014-08-28 22:08:47 +0100

    Get the link right.

--- lib/tickets.rb
+++ lib/tickets.rb
@@ -68,7 +68,7 @@ class Tickets
     response = @connection.post url, comment.to_json
     response_body = JSON.parse response.body
     audit(issue_reference, 'comment', {content: truncate_for_audit(text.sub(/\[\[USERNAME:(.+)\]\]\n\n/,''))})
-    Hipchat.notify('Support', "#{Authorization.current_user.email} replied to ticket <a href=\"https://datacentred.atlassian.net/browse/#{response_body['key']}\">#{issue_reference}</a>: #{truncate_for_audit(text.sub(/\[\[USERNAME:(.+)\]\]\n\n/,''))}")
+    Hipchat.notify('Support', "#{Authorization.current_user.email} replied to ticket <a href=\"https://datacentred.atlassian.net/browse/#{issue_reference}\">#{issue_reference}</a>: #{truncate_for_audit(text.sub(/\[\[USERNAME:(.+)\]\]\n\n/,''))}")
 
     response_body
   end
