ac3ae79e84d101a03e19b4c53d133c6374a40f32
Sean Handley <sean.handley@gmail.com>
2014-09-17 13:27:38 +0100

    Forget it.

--- lib/ticket_adapter.rb
+++ lib/ticket_adapter.rb
@@ -6,14 +6,34 @@ class TicketAdapter
 
   class << self
     def all
-      Rails.cache.fetch("tickets_#{Authorization.current_user.id}", expires_in: 30.seconds) { fetch_all }
+      total_pages = SIRPORTLY.request("tickets/contact", contact: Authorization.current_user.id)["pagination"]["pages"]
+      (1..total_pages).collect do |page|
+        SIRPORTLY.request("tickets/contact", contact: Authorization.current_user.id, page: page)["records"].map do |t|
+          head, *tail = SIRPORTLY.request("ticket_updates/all", ticket: t['reference'])
+          updates = tail.map do |u|
+            unless u['private']
+              TicketComment.new(ticket_reference: t['reference'], id: u['id'],
+                                email: u['from_address'], text: markdown(u['message']),
+                                time: Time.parse(u['posted_at']),
+                                staff: (u['author']["type"] == "User"),
+                                name: u['from_name'])
+            end
+          end.compact
+          description = (head ? head['message'] : nil)
+          Ticket.new(comments: updates, description: markdown(description),
+                     reference: t['reference'], title: t['subject'],
+                     created_at: Time.parse(t['submitted_at']),
+                     updated_at: Time.parse(t['updated_at']),
+                     email: t['customer_contact_method']['data'],
+                     name: extract_name_from_ticket(t),
+                     status: t['status'])
+        end
+      end.flatten
+    rescue Sirportly::Errors::NotFound
+      return []
     end
 
     def create(ticket)
-      # title = truncate_for_audit(ticket.title)
-      # description = truncate_for_audit(ticket.description)
-      # audit(reference, 'create', {title: title, description: description})
-
       properties = {
         :brand => 'DataCentred',
         :department => 'Support',
@@ -42,48 +62,7 @@ class TicketAdapter
       ticket = SIRPORTLY.ticket(reference)
       ticket.update(:status => status.downcase == 'open' ? 'New' : 'Resolved')
       Rails.cache.clear("tickets_#{Authorization.current_user.id}")
-      # audit(reference, 'update_status', {reference: reference, status: status})
     end
 
-    private
-
-    def fetch_all
-      total_pages = SIRPORTLY.request("tickets/contact", contact: Authorization.current_user.id)["pagination"]["pages"]
-      (1..total_pages).collect do |page|
-        SIRPORTLY.request("tickets/contact", contact: Authorization.current_user.id, page: page)["records"].map do |t|
-          head, *tail = SIRPORTLY.request("ticket_updates/all", ticket: t['reference'])
-          updates = tail.map do |u|
-            unless u['private']
-              TicketComment.new(ticket_reference: t['reference'], id: u['id'],
-                                email: u['from_address'], text: markdown(u['message']),
-                                time: Time.parse(u['posted_at']),
-                                staff: (u['author']["type"] == "User"),
-                                name: u['from_name'])
-            end
-          end.compact
-          description = (head ? head['message'] : nil)
-          Ticket.new(comments: updates, description: markdown(description),
-                     reference: t['reference'], title: t['subject'],
-                     created_at: Time.parse(t['submitted_at']),
-                     updated_at: Time.parse(t['updated_at']),
-                     email: t['customer_contact_method']['data'],
-                     name: extract_name_from_ticket(t),
-                     status: t['status'])
-        end
-      end.flatten
-    rescue Sirportly::Errors::NotFound
-      return []
-    end
-
-    def audit(id, action, params)
-      Audited::Adapters::ActiveRecord::Audit.create auditable_id: id, auditable_type: 'Ticket', action: action,
-                   user: Authorization.current_user,
-                   organization_id: Authorization.current_user.organization_id,
-                   audited_changes: params.stringify_keys!
-    end
-
-    def truncate_for_audit(content)
-      truncate(content, omission: '...', separator: '', length: 300)
-    end
   end
 end
