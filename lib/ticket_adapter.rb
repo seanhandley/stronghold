18e6f81b5cfdb20f007a1324f3e5099426e18410
Sean Handley <sean.handley@gmail.com>
2014-09-17 13:17:45 +0100

    Handle it when the user doesn't exist yet.

--- lib/ticket_adapter.rb
+++ lib/ticket_adapter.rb
@@ -48,31 +48,33 @@ class TicketAdapter
     private
 
     def fetch_all
-      total_pages = SIRPORTLY.request("tickets/contact", contact: Authorization.current_user.id)["pagination"]["pages"]
-      (1..total_pages).collect do |page|
-        SIRPORTLY.request("tickets/contact", contact: Authorization.current_user.id, page: page)["records"].map do |t|
-          head, *tail = SIRPORTLY.request("ticket_updates/all", ticket: t['reference'])
-          updates = tail.map do |u|
-            unless u['private']
-              TicketComment.new(ticket_reference: t['reference'], id: u['id'],
-                                email: u['from_address'], text: markdown(u['message']),
-                                time: Time.parse(u['posted_at']),
-                                staff: (u['author']["type"] == "User"),
-                                name: u['from_name'])
-            end
-          end.compact
-          description = (head ? head['message'] : nil)
-          Ticket.new(comments: updates, description: markdown(description),
-                     reference: t['reference'], title: t['subject'],
-                     created_at: Time.parse(t['submitted_at']),
-                     updated_at: Time.parse(t['updated_at']),
-                     email: t['customer_contact_method']['data'],
-                     name: extract_name_from_ticket(t),
-                     status: t['status'])
-        end
-      end.flatten
-    catch Sirportly::Errors::NotFound
-      return []
+      begin
+        total_pages = SIRPORTLY.request("tickets/contact", contact: Authorization.current_user.id)["pagination"]["pages"]
+        (1..total_pages).collect do |page|
+          SIRPORTLY.request("tickets/contact", contact: Authorization.current_user.id, page: page)["records"].map do |t|
+            head, *tail = SIRPORTLY.request("ticket_updates/all", ticket: t['reference'])
+            updates = tail.map do |u|
+              unless u['private']
+                TicketComment.new(ticket_reference: t['reference'], id: u['id'],
+                                  email: u['from_address'], text: markdown(u['message']),
+                                  time: Time.parse(u['posted_at']),
+                                  staff: (u['author']["type"] == "User"),
+                                  name: u['from_name'])
+              end
+            end.compact
+            description = (head ? head['message'] : nil)
+            Ticket.new(comments: updates, description: markdown(description),
+                       reference: t['reference'], title: t['subject'],
+                       created_at: Time.parse(t['submitted_at']),
+                       updated_at: Time.parse(t['updated_at']),
+                       email: t['customer_contact_method']['data'],
+                       name: extract_name_from_ticket(t),
+                       status: t['status'])
+          end
+        end.flatten
+      catch Sirportly::Errors::NotFound
+        return []
+      end
     end
 
     def audit(id, action, params)
