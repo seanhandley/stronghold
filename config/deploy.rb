a9c989e0685e5714f3ae6770f3c6c9661bb50d47
Sean Handley <sean.handley@gmail.com>
2014-09-24 12:10:45 +0100

    Update Capistrano to use 2.1.3.

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ set :deploy_to, '/home/rails/stronghold/'
 # set :keep_releases, 5
 
 set :rbenv_type, :user # or :system, depends on your rbenv setup
-set :rbenv_ruby, '2.1.2'
+set :rbenv_ruby, '2.1.3'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
