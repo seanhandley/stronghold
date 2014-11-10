ac4b57eb4d077d01461c7e136c591f7826cb269a
Sean Handley <sean.handley@gmail.com>
2014-11-10 15:07:35 +0000

    Don't set up virtual networking if it's a colo-only customer.

--- app/generators/customer_generator.rb
+++ app/generators/customer_generator.rb
@@ -23,7 +23,7 @@ class CustomerGenerator
         uuid = @organization.tenants.create(name: tenant).uuid
         OpenStack::Tenant.find(uuid).zero_quotas if @colo_only
       end
-      create_default_network(@organization)
+      create_default_network(@organization) unless @colo_only
       @invite = Invite.create! email: @email, power_invite: true, organization: @organization
       Mailer.signup(@invite.id).deliver
       return true
