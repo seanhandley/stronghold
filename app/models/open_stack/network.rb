c44866223a6eec85675f5b370fb41ad4b5242a44
Sean Handley <sean.handley@gmail.com>
2014-11-10 15:01:47 +0000

    Reference subnets from network.

--- app/models/open_stack/network.rb
+++ app/models/open_stack/network.rb
@@ -1,6 +1,15 @@
 module OpenStack
   class Network < OpenStackObject::Network
     attributes :name, :shared, :admin_state_up,
-               :tenant_id, :router_external
+               :tenant_id, :router_external, :subnets,
+               :network_id
+
+    def subnets
+      service_method do |s|
+        return s.subnets.select {|s| s.network_id == id }.map do |sn|
+          OpenStack::Subnet.new(sn)
+        end
+      end
+    end
   end
 end
