57bcf356f5d7dc67c78931b86667032be7b9f063
Sean Handley <sean.handley@gmail.com>
2014-11-10 17:49:43 +0000

    Set the subnet and IP on the port.

--- app/generators/customer_generator.rb
+++ app/generators/customer_generator.rb
@@ -43,7 +43,7 @@ class CustomerGenerator
       r = Fog::Network.new(OPENSTACK_ARGS).routers.create name: 'default', tenant_id: tenant_id,
                                    external_gateway_info: external_network.id
       Fog::Network.new(OPENSTACK_ARGS).ports.create name: 'default', network_id: n.id, device_id: r.id, 
-                             tenant_id: tenant_id
+                             tenant_id: tenant_id, fixed_ips: [{:subnet_id => s.id, :ip_address => '192.168.0.1'}]
     end
   end
 end
