d2db494f9b10d346ff77314607a018a5acf24902
Sean Handley <sean.handley@gmail.com>
2014-11-20 20:58:42 +0000

    Fix tests.

--- test/functional/billing/instances_test.rb
+++ test/functional/billing/instances_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class Billing::InstancesTest < Minitest::Test
   def setup
     @test_tenant_id = '1c483a77bbe44afcaf3a1d098a1a897f'
+    @first_sync = Billing::Sync.create(completed_at: DateTime.parse('2014-11-17 12:31:08'))
   end
 
   def test_sync
