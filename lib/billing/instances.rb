bbf5e611ace0bf6e669f866c3b210547c229ff29
Sean Handley <sean.handley@gmail.com>
2014-11-24 10:37:46 +0000

    No samples = no instance data.

--- lib/billing/instances.rb
+++ lib/billing/instances.rb
@@ -93,13 +93,6 @@ module Billing
         flavor_id = first_sample_metadata["instance_flavor_id"] ? first_sample_metadata["instance_flavor_id"] : first_sample_metadata["flavor.id"]
         instance = Billing::Instance.create(instance_id: instance_id, tenant_id: tenant_id, name: first_sample_metadata["display_name"],
                                  flavor_id: flavor_id, image_id: first_sample_metadata["image_ref_url"].split('/').last)
-        unless samples.any? {|s| s['resource_metadata']['event_type']}
-          # This is a new instance and we don't know its current state.
-          # Attempt to find out
-          if(os_instance = Fog::Compute.new(OPENSTACK_ARGS).servers.get(instance_id))
-            instance.instance_states.create recorded_at: DateTime.now, state: os_instance.state.downcase
-          end
-        end
       end
       billing_instance_id = Billing::Instance.find_by_instance_id(instance_id).id
       samples.collect do |s|
