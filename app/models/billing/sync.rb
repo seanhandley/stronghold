8d1dda9e9ea0f0e3784dcc445182135b66fe5a48
Sean Handley <sean.handley@gmail.com>
2014-12-04 13:30:02 +0000

    Clear syncs.

--- app/models/billing/sync.rb
+++ app/models/billing/sync.rb
@@ -5,5 +5,6 @@ module Billing
     has_many :instance_states, dependent: :destroy
     has_many :volume_states, dependent: :destroy
     has_many :ip_states, dependent: :destroy
+    has_many :image_states, dependent: :destroy
   end
 end
