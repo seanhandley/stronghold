ad7b44aa3ab48660433edf83fd818ed70acb6554
Sean Handley <sean.handley@gmail.com>
2014-12-11 11:33:56 +0000

    Ignore access denied errors.

--- config/initializers/honeybadger.rb
+++ config/initializers/honeybadger.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
+require 'cancancan'
 
 Honeybadger.configure do |config|
   config.api_key = '5a3dcd9f'
-  config.ignore  << Sinatra::NotFound
+  config.ignore  << [Sinatra::NotFound, CanCan::AccessDenied]
 end
