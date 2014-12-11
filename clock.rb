e092cd18605f5b95a69dd9ce38fd2c4dd11550d5
Sean Handley <sean.handley@gmail.com>
2014-12-11 11:43:02 +0000

    Because staging has weird data on it.

--- clock.rb
+++ clock.rb
@@ -3,8 +3,10 @@ require File.expand_path('config/boot', File.dirname(__FILE__))
 require File.expand_path('config/environment', File.dirname(__FILE__))
 include Clockwork
 
-every(30.minutes, 'usage_sync') do
-  UsageWorker.perform_async
+if Rails.env.production?
+  every(30.minutes, 'usage_sync') do
+    UsageWorker.perform_async
+  end
 end
 
 module Clockwork
