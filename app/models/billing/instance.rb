f95a0c6916057b35c7ccbbe99d593617ac16ab22
Sean Handley <sean.handley@gmail.com>
2015-02-27 10:04:07 +0000

    Enforce UUID uniqueness.

--- app/models/billing/instance.rb
+++ app/models/billing/instance.rb
@@ -2,6 +2,8 @@ module Billing
   class Instance < ActiveRecord::Base
     self.table_name = "billing_instances"
 
+    validates :instance_id, uniqueness: true
+
     has_many :instance_states
     belongs_to :instance_flavor, :class_name => "Billing::InstanceFlavor",
                :primary_key => 'flavor_id', :foreign_key => 'flavor_id'
