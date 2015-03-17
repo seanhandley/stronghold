e95e08fece74addb18aa961f715bac8cd72aea3e
Sean Handley <sean.handley@gmail.com>
2015-03-17 14:03:10 +0000

    Initial self service signup page.

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,8 +1,25 @@
 class SignupsController < ApplicationController
 
   layout 'sign-in'
+  layout 'customer-sign-up', only: [:new]
 
-  before_filter :find_invite
+  before_filter :find_invite, except: [:new, :create]
+  before_filter :get_products, only: [:new, :create]
+
+  def new
+    @customer_signup = CustomerSignupGenerator.new
+  end
+
+  def create
+    @customer_signup = CustomerSignupGenerator.new(create_params)
+    @organization = Organization.new(create_params[:organization])
+    if @customer_signup.generate!
+      redirect_to support_root_path
+    else
+      flash[:error] = @customer_signup.errors.full_messages.join('<br>')
+      render :new
+    end
+  end
 
   def edit
     reset_session
@@ -13,7 +30,7 @@ class SignupsController < ApplicationController
     @registration = RegistrationGenerator.new(@invite, update_params)
     if @registration.generate!
       session[:user_id] = @registration.user.id
-      session[:created_at] = Time.now
+      session[:created_at] = Time.zone.now
       redirect_to support_root_path, notice: 'Welcome!'
     else
       flash[:error] = @registration.errors.full_messages.join('<br>')
@@ -31,5 +48,14 @@ class SignupsController < ApplicationController
   def find_invite
     @invite = Invite.find_by_token(params[:token])
     raise ActionController::RoutingError.new('Not Found') unless @invite && @invite.can_register?
+  end
+
+  def create_params
+    params.permit(:organization_name, :email,
+                  :organization => {:product_ids => []})
+  end
+
+  def get_products
+    @products ||= Product.all
   end
 end
---
+++ app/generators/customer_signup_generator.rb
@@ -0,0 +1,85 @@
+class CustomerSignupGenerator
+  include ActiveModel::Validations
+
+  attr_reader :organization_name, :email, :first_name, :last_name,
+              :products, :password, :confirm_password,
+              :postcode, :privacy
+
+  def initialize(params={})
+    @organization_name = params[:organization_name]
+    @email             = params[:email]
+    @extra_tenants     = params[:extra_tenants]
+    if params[:organization] && params[:organization][:product_ids]
+      @products = params[:organization][:product_ids].select(&:present?)
+    else
+      @products = []
+    end
+  end
+
+  def generate!
+    if @organization_name.blank?
+      errors.add :base, "Must provide an organization name"
+    elsif @email.blank? || !(@email =~ /.+@.+\..+/)
+      errors.add :base, "Must provide a valid email address"
+    elsif User.find_by_email(@email).present?
+      errors.add :base, "Email already exists in the system"
+    elsif @products.none?
+      errors.add :base, "Select at least one product"
+    elsif @products.any? {|p| !Product.all.map(&:id).include?(p.to_i)}
+      errors.add :base, "Products invalid"
+    else
+      error = nil
+      ActiveRecord::Base.transaction do
+        begin
+          create_customer
+        rescue StandardError => e
+          error = e
+          raise ActiveRecord::Rollback
+        end
+      end
+
+      raise error if error
+      return true
+    end 
+    false
+  end
+
+  private
+
+  def create_customer
+    @organization = Organization.create! name: @organization_name
+    @products.each do |product_id|
+      @organization.products << Product.find(product_id)
+    end
+    @organization.save!
+    @extra_tenants.split(',').map(&:strip).map(&:downcase).uniq.each do |tenant|
+      @organization.tenants.create(name: tenant)
+    end
+    if colo_only?
+      @organization.tenants.each do |tenant|
+        OpenStack::Tenant.find(tenant.uuid).zero_quotas
+      end
+    end
+    create_default_network(@organization) unless colo_only?
+    @invite = Invite.create! email: @email, power_invite: true, organization: @organization
+    Mailer.signup(@invite.id).deliver_later
+  end
+
+  def colo_only?
+    products.collect{|p| Product.find(p).name}.include?('Colocation') && products.count == 1
+  end
+
+  def create_default_network(organization)
+    organization.tenants.collect(&:uuid).each do |tenant_id|
+      n = Fog::Network.new(OPENSTACK_ARGS).networks.create name: 'default', tenant_id: tenant_id
+      s = Fog::Network.new(OPENSTACK_ARGS).subnets.create name: 'default', cidr: '192.168.0.0/24',
+                                   network_id: n.id, ip_version: 4, dns_nameservers: ['8.8.8.8', '8.8.4.4'],
+                                   tenant_id: tenant_id
+      external_network = Fog::Network.new(OPENSTACK_ARGS).networks.select{|n| n.router_external == true }.first
+      r = Fog::Network.new(OPENSTACK_ARGS).routers.create name: 'default', tenant_id: tenant_id,
+                                   external_gateway_info: external_network.id
+      Fog::Network.new(OPENSTACK_ARGS).add_router_interface(r.id, s.id)
+
+    end
+  end
+end
