f6766e13c9d00e20a3f95fe939e8cf3d38042506
Sean Handley <sean.handley@gmail.com>
2015-03-20 06:26:24 +0000

    Name and password get set on confirmation email.

--- app/models/customer_signup.rb
+++ app/models/customer_signup.rb
@@ -1,23 +1,8 @@
 class CustomerSignup < ActiveRecord::Base
-
-  attr_reader :password
-
   after_create -> { update_attributes(uuid: SecureRandom.hex) }
-  after_create -> { Rails.cache.write(cache_id, password, expires_in: 1.hour) }
 
-  validates :first_name, :last_name,
-            :password, presence: true
   validates :email, length: {minimum: 5}, allow_blank: false
-  validate :email_valid, :password_complexity, :passwords_match
-
-  def initialize(params)
-    @password, @confirm_password = params.delete(:password), params.delete(:confirm_password)
-    super
-  end
-
-  def password
-    @password || Rails.cache.fetch(cache_id)
-  end
+  validate :email_valid
 
   def expired?
     created_at + 1.hour < Time.zone.now
@@ -33,13 +18,4 @@ class CustomerSignup < ActiveRecord::Base
     errors.add(:email, I18n.t(:is_not_a_valid_address)) unless email =~ /.+@.+\..+/
   end
 
-  def passwords_match
-    unless @password && @confirm_password && @password == @confirm_password
-      errors.add(:base, "Password doesn't match confirmation")
-    end
-  end
-
-  def password_complexity
-    errors.add(:base, "Password is too short") unless @password && @password.length >= 8
-  end
 end
--- db/migrate/20150319090132_create_customer_signups.rb
+++ db/migrate/20150319090132_create_customer_signups.rb
@@ -3,8 +3,6 @@ class CreateCustomerSignups < ActiveRecord::Migration
     create_table :customer_signups do |t|
       t.string :uuid
       t.string :email
-      t.string :first_name
-      t.string :last_name
       t.string :organization_name
       t.boolean :payment_verified, default: false, null: false
       t.timestamps
