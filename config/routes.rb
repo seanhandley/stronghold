62d2c8d8413da87537da1108fec2cae186c0f2b5
Sean Handley <sean.handley@gmail.com>
2015-03-26 14:35:08 +0000

    Wording changes.

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,7 @@ Rails.application.routes.draw do
   post 'signup/:token', :controller => 'signups', :action => 'update', :as => 'signup_complete'
   get 'signup', :controller => 'signups', :action => 'new', :as => 'new_signup'
   post 'signup', :controller => 'signups', :action => 'create', :as => 'create_signup'
-  post 'pay', :controller => 'signups', :action => 'take_payment', :as => 'take_payment'
+  get 'pay', :controller => 'signups', :action => 'take_payment', :as => 'take_payment'
   post 'precheck', :controller => 'signups', :action => 'precheck'
 
   post 'salesforce', :controller => 'salesforce', :action => 'update'
