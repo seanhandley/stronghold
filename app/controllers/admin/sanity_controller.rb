1981e82aca1f7d20c6efa8a79e992aa6a8dd4e31
Sean Handley <sean.handley@gmail.com>
2015-04-03 16:18:02 +0100

    Breakdown of billing syncs.

--- app/controllers/admin/sanity_controller.rb
+++ app/controllers/admin/sanity_controller.rb
@@ -1,8 +1,8 @@
 class Admin::SanityController < AdminBaseController
 
   def index
-    syncs = Billing::Sync.where("completed_at > ?", Time.zone.now - 24.hours)
-    @total = syncs.collect{|s| s.instance_states.count + s.volume_states.count + s.ip_states.count + s.image_states.count }.sum
+    @syncs = Billing::Sync.where("completed_at > ?", Time.zone.now - 24.hours)
+    @total = @syncs.collect{|s| s.instance_states.count + s.volume_states.count + s.ip_states.count + s.image_states.count }.sum
     @keys = ['instances', 'volumes', 'images', 'routers']
     @data = Sanity.check
   end
