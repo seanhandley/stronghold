1a094b5d38eb44f343edcc1c0d4e82382bad306c
Sean Handley <sean.handley@gmail.com>
2015-04-03 20:15:42 +0100

    Do UTC during the query.

--- lib/billing.rb
+++ lib/billing.rb
@@ -4,9 +4,9 @@ module Billing
 
   def self.sync!
     ActiveRecord::Base.transaction do
-      from = Billing::Sync.last.started_at.utc
-      to   = Time.now.utc
-      sync = Billing::Sync.create started_at: Time.now.utc
+      from = Billing::Sync.last.started_at
+      to   = Time.now
+      sync = Billing::Sync.create started_at: Time.now
       Billing::Instances.sync!(from, to, sync)
       Billing::Volumes.sync!(from, to, sync)
       #Billing::FloatingIps.sync!(from, to, sync)
@@ -14,15 +14,15 @@ module Billing
       Billing::ExternalGateways.sync!(from, to, sync)
       Billing::Images.sync!(from, to, sync)
       Billing::StorageObjects.sync!(sync)
-      sync.update_attributes(completed_at: Time.now.utc)
+      sync.update_attributes(completed_at: Time.now)
       #raise ActiveRecord::Rollback
     end
   end
 
   def self.fetch_samples(tenant_id, measurement, from, to)
     timestamp_format = "%Y-%m-%dT%H:%M:%S"
-    options = [{'field' => 'timestamp', 'op' => 'ge', 'value' => from.strftime(timestamp_format)},
-               {'field' => 'timestamp', 'op' => 'lt', 'value' => to.strftime(timestamp_format)},
+    options = [{'field' => 'timestamp', 'op' => 'ge', 'value' => from.utc.strftime(timestamp_format)},
+               {'field' => 'timestamp', 'op' => 'lt', 'value' => to.utc.strftime(timestamp_format)},
                {'field' => 'project_id', 'value' => tenant_id, 'op' => 'eq'}]
     tenant_samples = Fog::Metering.new(OPENSTACK_ARGS).get_samples(measurement, options).body
     tenant_samples.group_by{|s| s['resource_id']}
