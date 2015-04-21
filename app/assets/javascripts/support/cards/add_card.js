9a24693aca917ee314ca9ef5e831d4c944814128
Sean Handley <sean.handley@gmail.com>
2015-04-21 07:49:24 +0100

    This is why namespacing in JS is important.

--- app/assets/javascripts/support/cards/add_card.js
+++ app/assets/javascripts/support/cards/add_card.js
@@ -4,36 +4,38 @@ $(document).ready(function() {
 
     $form.find('input[type=submit]').prop('disabled', true);
 
-    Stripe.card.createToken($form, stripeResponseHandler);
+    Stripe.card.createToken($form, AddCard.stripeResponseHandler);
 
     return false;
   });
 });
 
-function stripeResponseHandler(status, response) {
-  var $form = $('#add_new_card');
+var AddCard = {
+  stripeResponseHandler: function (status, response) {
+    var $form = $('#add_new_card');
 
-  document.body.style.cursor='wait';
+    document.body.style.cursor='wait';
 
-  if (response.error) {
-    showError(response.error.message);
-    $form.find('input[type=submit]').prop('disabled', false);
-    document.body.style.cursor='default';
-  } else {
-    // response contains id and card, which contains additional card details
-    var token = response.id;
-    // Insert the token into the form so it gets submitted to the server
-    $form.append($('<input type="hidden" name="stripe_token" />').val(token));
-    $form.get(0).submit();
+    if (response.error) {
+      AddCard.showError(response.error.message);
+      $form.find('input[type=submit]').prop('disabled', false);
+      document.body.style.cursor='default';
+    } else {
+      // response contains id and card, which contains additional card details
+      var token = response.id;
+      // Insert the token into the form so it gets submitted to the server
+      $form.append($('<input type="hidden" name="stripe_token" />').val(token));
+      $form.get(0).submit();
+    }
+  },
+
+  showError: function (message) {
+    // Show the errors on the form
+    var $form = $('#add_new_card');
+    var $div = $("<div id='flash'></div>");
+    $div.append($("<p>" + message + "</p>").addClass('error'));
+    $form.find('#flashes').empty();
+    $form.find('#flashes').removeClass('hide');
+    $form.find('#flashes').append($div)
   }
-};
-
-function showError(message) {
-  // Show the errors on the form
-  var $form = $('#add_new_card');
-  var $div = $("<div id='flash'></div>");
-  $div.append($("<p>" + message + "</p>").addClass('error'));
-  $form.find('#flashes').empty();
-  $form.find('#flashes').removeClass('hide');
-  $form.find('#flashes').append($div)
 }
