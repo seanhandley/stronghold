48713a3c9333ce9a8fa81284756dcc13bd8f5f90
Sean Handley <sean.handley@gmail.com>
2015-04-27 09:30:31 +0100

    Zero is not a valid usage limit.

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -10,7 +10,7 @@ class Voucher < ActiveRecord::Base
             presence: true, allow_blank: false
 
   validates :code, uniqueness: true
-  validates :duration, numericality: { only_integer: true, greater_than: 0 }
+  validates :duration, :usage_limit, numericality: { only_integer: true, greater_than: 0 }
   validates :discount_percent, numericality: { greater_than: 0, less_than_or_equal_to: 100 }
   validate -> { errors.add(:expires_at, "is in the past") if expired? }, on: :create
 
