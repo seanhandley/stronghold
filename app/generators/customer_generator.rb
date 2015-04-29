d0b2b6c4281cb02f80b1a2b77fc1ad40ddbf6483
Sean Handley <sean.handley@gmail.com>
2015-04-29 21:05:07 +0100

    Accounts room.

--- app/generators/customer_generator.rb
+++ app/generators/customer_generator.rb
@@ -61,7 +61,7 @@ class CustomerGenerator
     create_default_network(@organization) unless colo_only?
     @invite = Invite.create! email: @email, power_invite: true, organization: @organization
     Mailer.signup(@invite.id).deliver_later
-    Hipchat.notify('Internal Signup', 'Signups', "New customer account created: #{@email} invited to organization #{@organization_name}")
+    Hipchat.notify('Internal Signup', 'Accounts', "New customer account created: #{@email} invited to organization #{@organization_name}")
   end
 
   def colo_only?
