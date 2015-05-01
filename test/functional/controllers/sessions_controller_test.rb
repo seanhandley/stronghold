b0db9ad485f6e9d72794f07d2de34b51af442427
Sean Handley <sean.handley@gmail.com>
2015-05-01 11:56:53 +0100

    Testing.

--- test/functional/controllers/sessions_controller_test.rb
+++ test/functional/controllers/sessions_controller_test.rb
@@ -3,13 +3,11 @@ require 'test_helper'
 class SessionsControllerTest < ActionController::TestCase
   setup do
     @user = User.make!
-    session[:user_id]    = @user.id
-    session[:created_at] = Time.now.utc
-    session[:token]      = SecureRandom.hex
     @organization        = @user.organization
   end
 
   test "cannot access new, index, create when logged in" do
+    log_in
     @organization.stub(:has_payment_method?, true) do
       @organization.stub(:known_to_payment_gateway?, true) do
         [:new, :index, :create].each do |action|
@@ -22,8 +20,19 @@ class SessionsControllerTest < ActionController::TestCase
   end
 
   test "can log out" do
+    log_in
+    assert session.keys.count > 1
     delete :destroy, id: @user.id
     assert_redirected_to sign_in_path
-    assert_nil session[:user_id]
+    assert_equal 1, session.keys.count
+    assert_equal "You have been signed out.", session["flash"]["flashes"]["notice"]
+  end
+
+  private
+
+  def log_in
+    session[:user_id]    = @user.id
+    session[:created_at] = Time.now.utc
+    session[:token]      = SecureRandom.hex
   end
 end
