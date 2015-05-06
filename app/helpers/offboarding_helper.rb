fab7bb89f8a265d911668e969ca8093904c16e55
Sean Handley <sean.handley@gmail.com>
2015-05-06 13:23:04 +0100

    Clear images, volumes and snapshots.

--- app/helpers/offboarding_helper.rb
+++ app/helpers/offboarding_helper.rb
@@ -9,6 +9,25 @@ module OffboardingHelper
       fog.delete_server(instance)
     end
 
+    images = fog.list_images_detail(tenant_id: tenant.uuid).body['images'].map{|i| i['id']}
+    images.each do |image|
+      begin
+        fog.delete_image(image)
+      rescue Excon::Errors::Forbidden
+      end
+    end
+
+    fog = Fog::Volume.new(OPENSTACK_ARGS)
+    snapshots = fog.list_snapshots(true, :all_tenants => true).body['snapshots'].select{|s| s["os-extended-snapshot-attributes:project_id"] == tenant.uuid}.map{|s| s['id']}
+    snapshots.each do |snapshot|
+      fog.delete_snapshot(snapshot)
+    end
+
+    volumes = fog.list_volumes(true, :all_tenants => true).body['volumes'].select{|v| v["os-vol-tenant-attr:tenant_id"] == tenant.uuid}.map{|v| v['id']}
+    volumes.each do |volume|
+      fog.delete_volume(volume)
+    end
+
     fog = Fog::Network.new(OPENSTACK_ARGS)
     routers  = fog.list_routers(tenant_id:  tenant.uuid).body['routers'].map{|r| r['id']}
     subnets  = fog.list_subnets(tenant_id:  tenant.uuid).body['subnets'].map{|s| s['id']}
