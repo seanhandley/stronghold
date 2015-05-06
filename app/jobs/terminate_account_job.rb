94f357abc882cd432506a71319e8c2c75eb3fe22
Sean Handley <sean.handley@gmail.com>
2015-05-06 09:35:16 +0100

    Don't delete staff org!

--- app/jobs/terminate_account_job.rb
+++ app/jobs/terminate_account_job.rb
@@ -2,7 +2,7 @@ class TerminateAccountJob < ActiveJob::Base
   include OffboardingHelper
   queue_as :default
 
-  def perform(organization)
-    organization.tenants.each {|tenant| offboard(tenant_uuid) }
+  def perform(tenants)
+    tenants.each {|tenant| offboard(tenant_uuid) }
   end
 end
