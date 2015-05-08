c860dd69a62cfc8e93412447a27c37e933f15d1d
Sean Handley <sean.handley@gmail.com>
2015-05-08 14:37:00 +0100

    Sync if production.

--- lib/active_record/salesforce.rb
+++ lib/active_record/salesforce.rb
@@ -25,7 +25,7 @@ module ActiveRecord
       end
 
       self.class_eval do
-        if Rails.env.development?
+        if Rails.env.production?
           after_commit(:create_salesforce_object, on: :create)
           after_commit(:update_salesforce_object, on: :update)
         end
