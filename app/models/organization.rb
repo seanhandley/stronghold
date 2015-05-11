b591eced54aa4a09a10b15d7010084539d9976ff
Sean Handley <sean.handley@gmail.com>
2015-05-11 08:11:16 +0100

    Disable all tenants.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -81,6 +81,9 @@ class Organization < ActiveRecord::Base
   end
 
   def disable!
+    tenants.each do |tenant|
+      Fog::Identity.new(OPENSTACK_ARGS).update_tenant(tenant.uuid, enabled: false)
+    end
     users.each do |user|
       # Enable the user
       Fog::Identity.new(OPENSTACK_ARGS).update_user(user.uuid, enabled: false)
