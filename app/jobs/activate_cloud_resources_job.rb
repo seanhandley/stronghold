24ee2513225212d0eef6ba8d99ce0934605a3bcd
Sean Handley <sean.handley@gmail.com>
2015-05-12 11:24:05 +0100

    Faster

--- app/jobs/activate_cloud_resources_job.rb
+++ app/jobs/activate_cloud_resources_job.rb
@@ -2,6 +2,7 @@ class ActivateCloudResourcesJob < ActiveJob::Base
   queue_as :default
 
   def perform(organization)
+    organization.send :enable!
     organization.send :create_default_network!
     organization.send :set_quotas!
   end
