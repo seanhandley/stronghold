2b2733617bc13d83d50298a81304b28ced754af6
Sean Handley <sean.handley@gmail.com>
2015-05-17 21:32:29 +0100

    Revert "Click again to toggle back."
    This reverts commit 49f78ad43a235ebc21de1e5a748d964ccdee042b.

--- app/assets/javascripts/click_to_reveal.js
+++ app/assets/javascripts/click_to_reveal.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
   $('.click-to-reveal').on('click', function(){
-    var reveal = $(this).data('reveal');
-    var text = $(this).html()
-    $(this).html(reveal);
-    $(this).data({'reveal': text});
+    $(this).text($(this).data('reveal'));
   });
 });
