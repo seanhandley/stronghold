9d90533d012a17cfd86890c4a6373ae65e4ff305
Sean Handley <sean.handley@gmail.com>
2015-05-19 09:56:41 +0100

    Only make ec2 credentials on primary tenant.

--- app/jobs/create_ec2_credentials_job.rb
+++ app/jobs/create_ec2_credentials_job.rb
@@ -2,8 +2,7 @@ class CreateEC2CredentialsJob < ActiveJob::Base
   queue_as :default
 
   def perform(user)
-    user.organization.tenants.each do |tenant|
-      credential = Fog::Identity.new(OPENSTACK_ARGS).create_ec2_credential(user.uuid, tenant.uuid).body['credential']
-    end
+    tenant = user.organization.primary_tenant
+    Fog::Identity.new(OPENSTACK_ARGS).create_ec2_credential(user.uuid, tenant.uuid).body['credential']
   end
 end
