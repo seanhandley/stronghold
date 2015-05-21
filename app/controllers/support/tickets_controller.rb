5cbda50791506e4be9848e28610c2e5da6464fc2
Sean Handley <sean.handley@gmail.com>
2015-05-21 12:22:33 +0100

    Don't let non authed customers raise emergency alerts.

--- app/controllers/support/tickets_controller.rb
+++ app/controllers/support/tickets_controller.rb
@@ -30,6 +30,7 @@ class Support::TicketsController < SupportBaseController
   def get_departments_and_priorities
     @departments = TicketAdapter.departments
     @priorities  = TicketAdapter.priorities
+    @priorities.delete('Emergency') unless current_user.organization.known_to_payment_gateway?
     unless current_user.organization.colo? && current_user.has_permission?('access_requests.modify')
       @departments -= ['Access Requests']
     end
