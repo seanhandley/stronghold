7418df0aa988455eb98ab99b9b291bb263a75ccb
Sean Handley <sean.handley@gmail.com>
2015-05-22 10:10:13 +0100

    Beautiful emails.

--- test/mailers/previews/mailer_preview.rb
+++ test/mailers/previews/mailer_preview.rb
@@ -4,10 +4,35 @@ class MailerPreview < ActionMailer::Preview
   end
 
   def usage_report
-    Mailer.usage_report(Time.now - 7.days, Time.now)
+    Mailer.usage_report((Time.now - 7.days).to_s, Time.now.to_s)
   end
 
   def usage_sanity_failures
     Mailer.usage_sanity_failures(Sanity.check)
+  end
+
+  def activation_reminder
+    Mailer.activation_reminder('foo@bar.com')
+  end
+
+  def reset
+    Mailer.reset(Reset.first.id)
+  end
+
+  def fraud_check_alert
+    Mailer.fraud_check_alert({}, 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+  end
+
+  def card_reverification_failure
+    Mailer.card_reverification_failure(Organization.first)
+  end
+
+
+  def notify_wait_list_entry
+    Mailer.notify_wait_list_entry('foo@bar.com')
+  end
+
+  def goodbye
+    Mailer.goodbye(Organization.first.admin_users)
   end
 end
