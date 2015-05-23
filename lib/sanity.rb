796cc1492c1d67b38f92644fcc06238f7ff8a8ac
Sean Handley <sean.handley@gmail.com>
2015-05-23 07:11:24 +0100

    Premailer changes this.

--- lib/sanity.rb
+++ lib/sanity.rb
@@ -40,7 +40,7 @@ module Sanity
   end
 
   def self.notify!(data)
-    msg = Mailer.usage_sanity_failures(data).html_part.body.raw_source.gsub("\n","<br />").strip
+    msg = Mailer.usage_sanity_failures(data).body.raw_source.gsub("\n","<br />").strip
     Hipchat.notify('Sanity Check', 'Web', msg, :color => 'red')
   end
 
