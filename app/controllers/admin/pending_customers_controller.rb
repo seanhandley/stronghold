254f45e86e41b3b324a0704e600c9ceb03a8be25
Sean Handley <sean.handley@gmail.com>
2015-06-10 15:01:39 +0100

    Hipchat notify.

--- app/controllers/admin/pending_customers_controller.rb
+++ app/controllers/admin/pending_customers_controller.rb
@@ -9,6 +9,9 @@ class Admin::PendingCustomersController < AdminBaseController
     if organization.manually_activate!
       Announcement.create(title: 'Welcome', body: 'Your identity is verified and you may now begin using cloud services!',
         limit_field: 'id', limit_value: organization.users.first.id)
+      Hipchat.notify('Self Service', 'Accounts',
+                     "(goodnews) Good news! #{organization.name} is a paying customer! (Manually activated by #{current_user.name})",
+                     color: 'green', message_format: 'text')
       redirect_to admin_pending_customers_path, notice: 'Success!'
     else
       redirect_to admin_pending_customers_path, alert: 'Failed to activate...'
