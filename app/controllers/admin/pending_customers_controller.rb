f6019012a8c648365390117e856142aa572db4a8
Sean Handley <sean.handley@gmail.com>
2015-06-11 10:17:11 +0100

    Use a generic Notifications wrapper for Hipchat and Slack.

--- app/controllers/admin/pending_customers_controller.rb
+++ app/controllers/admin/pending_customers_controller.rb
@@ -9,9 +9,8 @@ class Admin::PendingCustomersController < AdminBaseController
     if organization.manually_activate!
       Announcement.create(title: 'Welcome', body: 'Your identity is verified and you may now begin using cloud services!',
         limit_field: 'id', limit_value: organization.users.first.id)
-      Hipchat.notify('Self Service', 'Accounts',
-                     "(goodnews) Good news! #{organization.name} is a paying customer! (Manually activated by #{current_user.name})",
-                     color: 'green', message_format: 'text')
+
+      Notifications.notify(:new_signup, "#{organization.name} is a paying customer! (Manually activated by #{current_user.name})")
       redirect_to admin_pending_customers_path, notice: 'Success!'
     else
       redirect_to admin_pending_customers_path, alert: 'Failed to activate...'
