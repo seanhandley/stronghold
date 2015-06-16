7b6489960b864f7f4e0f5eb4679e2931b6c51ece
Sean Handley <sean.handley@gmail.com>
2015-06-16 12:18:26 +0100

    Get it right.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,6 +96,7 @@ Rails.application.configure do
   }
 
   config.action_controller.asset_host = "https://my.datacentred.io"
+  config.action_mailer.asset_host = config.action_controller.asset_host
   config.action_mailer.default_url_options = { :host => 'my.datacentred.io' }
 
   # Error handling
