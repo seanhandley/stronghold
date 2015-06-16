d115281e0a055f3e15f597d56f61e917c1be353e
Sean Handley <sean.handley@gmail.com>
2015-06-16 08:32:50 +0100

    Don't recreate the customer each time.

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -4,19 +4,21 @@ class StripeController < ApplicationController
   def precheck
     rescue_stripe_errors(lambda {|msg| render json: {success: false, message: msg}}) do
       @customer_signup = CustomerSignup.find_by_uuid(payment_params[:signup_uuid])
-      customer = Stripe::Customer.create(
-        :source => payment_params[:stripe_token],
-        :email => @customer_signup.email,
-        :description => "Company: #{@customer_signup.organization_name}, Signup UUID: #{@customer_signup.uuid}"
-      )
+      unless @customer_signup.stripe_customer_id
+        customer = Stripe::Customer.create(
+          :source => payment_params[:stripe_token],
+          :email => @customer_signup.email,
+          :description => "Company: #{@customer_signup.organization_name}, Signup UUID: #{@customer_signup.uuid}"
+        )
 
-      @customer_signup.update_attributes(stripe_customer_id: customer.id)
+        @customer_signup.update_attributes(stripe_customer_id: customer.id)
+      end
 
       if @customer_signup.ready?    
         render json: {success: true, message: ''}
       else
         # customer.delete
-        render json: {success: false, message: 'The address does not match the card'}
+        render json: {success: false, message: 'The address does not match the card. Try activating by phone?'}
       end
     end
   end
