f19d043357a3a37f057aed0a63559840489fa35c
Sean Handley <sean.handley@gmail.com>
2015-06-22 15:12:04 +0100

    Show the cost for instances.

--- lib/billing/instances.rb
+++ lib/billing/instances.rb
@@ -24,7 +24,8 @@ module Billing
                                          name: instance.instance_flavor.name,
                                          vcpus_count: instance.instance_flavor.vcpus,
                                          ram_mb: instance.instance_flavor.ram,
-                                         root_disk_gb: instance.instance_flavor.disk},
+                                         root_disk_gb: instance.instance_flavor.disk,
+                                         rate: instance.rate},
                                        image_id: instance.image_id}
       end
       instances.select{|i| i[:billable_seconds] > 0}
