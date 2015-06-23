509982f9ba53b593a7e27dc813337048fb5a24fe
Sean Handley <sean.handley@gmail.com>
2015-06-23 20:26:43 +0100

    Make this a little more durable.

--- lib/fraud_check.rb
+++ lib/fraud_check.rb
@@ -2,18 +2,23 @@ module FraudCheck
   def self.looks_suspicious?(customer_signup)
     fields = {
       :client_ip => customer_signup.real_ip, 
-      :city => customer_signup.organization.billing_city, 
-      :postal => customer_signup.organization.billing_postcode, 
-      :country => customer_signup.organization.billing_country, 
       :forwarded_ip => customer_signup.forwarded_ip,
       :email => customer_signup.email,
-      :shipping_address => [customer_signup.organization.billing_address1, customer_signup.organization.billing_address2].join("\n"),
-      :shipping_city => customer_signup.organization.billing_city, 
-      :shipping_postal => customer_signup.organization.billing_postcode, 
-      :shipping_country => customer_signup.organization.billing_country, 
       :user_agent => customer_signup.user_agent,
       :accept_language => customer_signup.accept_language
     }
+    if customer_signup.organization
+      org_fields = {
+        :city => customer_signup.organization.billing_city, 
+        :postal => customer_signup.organization.billing_postcode, 
+        :country => customer_signup.organization.billing_country, 
+        :shipping_address => [customer_signup.organization.billing_address1, customer_signup.organization.billing_address2].join("\n"),
+        :shipping_city => customer_signup.organization.billing_city, 
+        :shipping_postal => customer_signup.organization.billing_postcode, 
+        :shipping_country => customer_signup.organization.billing_country, 
+      }
+      fields.merge!(org_fields)
+    end
     request = Maxmind::Request.new(fields)
     response = request.process!
     # response.attributes = {:distance=>90, :country_match=>true, :country_code=>"DE", :free_mail=>false,
