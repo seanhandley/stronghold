9f829acdee850d9d38a2025fc3cb5c11bdde9fe2
Sean Handley <sean.handley@gmail.com>
2015-06-24 15:16:01 +0100

    Don't use old fraud check.

--- app/generators/registration_generator.rb
+++ app/generators/registration_generator.rb
@@ -43,13 +43,6 @@ class RegistrationGenerator
     @user = @organization.users.create email: invite.email.downcase, password: password,
                                        roles: roles
     @user.save!
-    unless Rails.env.test?
-      FraudCheckJob.perform_later({
-        name: @user.name,
-        company: @organization.name,
-        email: @user.email
-      }.merge(@organization.customer_signup ? {ip: @organization.customer_signup.ip_address} : {}))
-    end
     OpenStack::User.update_enabled(@user.uuid, false) unless @organization.has_payment_method?
     if invite.power_invite?
       member_uuid = OpenStack::Role.all.select{|r| r.name == '_member_'}.first.id
