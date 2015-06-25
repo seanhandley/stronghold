8554a5bfa08caaeeeeaf87e5e3339e84df766479
Sean Handley <sean.handley@gmail.com>
2015-06-25 09:28:12 +0100

    Don't do fraud checks in test envs.

--- app/jobs/fraud_check_job.rb
+++ app/jobs/fraud_check_job.rb
@@ -2,11 +2,13 @@ class FraudCheckJob < ActiveJob::Base
   queue_as :default
 
   def perform(customer_signup)
-    fc = FraudCheck.new(customer_signup)
-    if fc.suspicious?
-      customer_signup.organization.hard_freeze!
-      Mailer.fraud_check_alert(customer_signup, fc).deliver_later
-      Mailer.review_mode_alert(customer_signup).deliver_later
+    unless Rails.env.test? || Rails.env.acceptance?
+      fc = FraudCheck.new(customer_signup)
+      if fc.suspicious?
+        customer_signup.organization.hard_freeze!
+        Mailer.fraud_check_alert(customer_signup, fc).deliver_later
+        Mailer.review_mode_alert(customer_signup).deliver_later
+      end
     end
   end
 end
