1521ad28b493fd8dc44f9f12bfc132fd5e211923
Sean Handley <sean.handley@gmail.com>
2015-07-01 09:57:41 +0100

    Nil guard.

--- lib/fraud_check.rb
+++ lib/fraud_check.rb
@@ -29,6 +29,7 @@ class FraudCheck
   end
 
   def card_fields
+    return nil unless customer_signup.stripe_customer
     card = customer_signup.stripe_customer.sources.data.first
     {
       number: "*** *** *** #{card.last4}",
