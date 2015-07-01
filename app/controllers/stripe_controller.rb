b4de57fa97300356eb4018b0f76ab69dbe100acb
Sean Handley <sean.handley@gmail.com>
2015-07-01 14:34:28 +0100

    Handle unknown customers.

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -1,10 +1,12 @@
+require 'ostruct'
+
 class StripeController < ApplicationController
   protect_from_forgery :except => :webhook
 
   def webhook
 
     if params["type"] == "invoice.payment_succeeded"
-      customer = Stripe::Customer.retrieve(params['data']['object']['customer'])
+      customer = Stripe::Customer.retrieve(params['data']['object']['customer']) rescue OpenStruct.new(description: "unknown")
       total = (params["data"]["object"]["total"] / 100.0).round(2)
       total = "%.2f" % (total)
       Notifications.notify(:new_signup, "Customer invoiced for £#{total}. #{customer.description}.")
