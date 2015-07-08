617cf7e3a59f3a91396830cb458778d108f3c415
Sean Handley <sean.handley@gmail.com>
2015-07-08 08:09:21 +0100

    Should catch any load issues with tickets.

---
+++ test/acceptance/unactivated_cloud_users/support_tickets_test.rb
@@ -0,0 +1,15 @@
+require_relative '../acceptance_test_helper'
+
+class SupportTicketsTests < CapybaraTestCase
+  
+  def test_support_tickets_loads_ok
+    visit(support_tickets_path)
+
+    sleep(5)
+
+    within('div.some-info') do
+      assert has_content?('There are no tickets')
+    end
+  end
+  
+end
