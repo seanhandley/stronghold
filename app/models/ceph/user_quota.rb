2742e2022aa0c2606e813760b32a2d64548c6da0
Sean Handley <sean.handley@gmail.com>
2015-07-16 08:58:16 +0100

    Set user quota.

--- app/models/ceph/user_quota.rb
+++ app/models/ceph/user_quota.rb
@@ -1,4 +1,22 @@
 module Ceph
   class UserQuota < CephObject::UserQuota
+    LIMITED_QUOTA_SIZE_KB = 1000 ** 3 * 5 # 5GB
+    def self.set_limited(uid)
+      set_quota(uid, 'enabled' => true, 'max_size_kb' => LIMITED_QUOTA_SIZE_KB, 'max_objects' => -1)
+    end
+
+    def self.set_unlimited(uid)
+      set_quota(uid, 'enabled' => false, 'max_size_kb' => -1, 'max_objects' => -1)
+    end
+
+    private
+
+    def set_quota(uid, body)
+      CephObject::UserQuota.create(default_params.merge('uid' => uid), body.to_json)
+    end
+
+    def self.default_params
+      {'quota-type' => 'user'}
+    end
   end
 end
--- lib/ceph_object/subclasses.rb
+++ lib/ceph_object/subclasses.rb
@@ -11,14 +11,6 @@ module CephObject
     end
     class UserQuota < Base
       def self.path; '/admin/user?quota&' ; end
-      def self.get(params)
-        super params.merge('quota-type' => 'user')
-      end
-      # Ceph::UserQuota.create 'uid' => '...', 'size' => 12345
-      def self.create(params)
-        body = {'enabled' =>true, 'max_size_kb' => params['size'], 'max_objects' => -1}
-        super params.merge('quota-type' => 'user'), body.to_json
-      end
     end
   end
 end
