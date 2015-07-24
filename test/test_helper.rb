1e172d125c547923c1970689086d2365db8c1cbc
Sean Handley <sean.handley@gmail.com>
2015-07-24 10:45:24 +0100

    Test harness for instance costs.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -47,24 +47,71 @@ class ActiveSupport::TestCase
 end
 
 def load_instance_flavors
-  Billing::InstanceFlavor.create(flavor_id: '1', name: 'm1.tiny', ram: 512, disk: 1, vcpus: 1)
-  Billing::InstanceFlavor.create(flavor_id: '2', name: 'm1.small', ram: 2048, disk: 20, vcpus: 1)
-  Billing::InstanceFlavor.create(flavor_id: '3', name: 'm1.medium', ram: 4096, disk: 40, vcpus: 2)
-  Billing::InstanceFlavor.create(flavor_id: '5', name: 'm1.xlarge', ram: 16384, disk: 160, vcpus: 8)
-  Billing::InstanceFlavor.create(flavor_id: '4', name: 'm1.large', ram: 8192, disk: 80, vcpus: 4)
+  Billing::InstanceFlavor.create(id: 1, flavor_id: '1', name: 'm1.tiny', ram: 512, disk: 1, vcpus: 1)
+  Billing::InstanceFlavor.create(id: 2, flavor_id: '2', name: 'm1.small', ram: 2048, disk: 20, vcpus: 1)
+  Billing::InstanceFlavor.create(id: 3, flavor_id: '3', name: 'm1.medium', ram: 4096, disk: 40, vcpus: 2)
+  Billing::InstanceFlavor.create(id: 4, flavor_id: '5', name: 'm1.xlarge', ram: 16384, disk: 160, vcpus: 8)
+  Billing::InstanceFlavor.create(id: 5, flavor_id: '4', name: 'm1.large', ram: 8192, disk: 80, vcpus: 4)
 
-  Billing::InstanceFlavor.create(flavor_id: 'ede87d00-b2a7-4b74-acf7-1490a6c29f22', name: 'm1.xlarge', ram: 16384, disk: 80, vcpus: 8)
-  Billing::InstanceFlavor.create(flavor_id: 'c3ac228b-d74e-4e22-88a2-773d86b96469', name: 'dc1.8x16', ram: 16384, disk: 120, vcpus: 8)
-  Billing::InstanceFlavor.create(flavor_id: 'f0577618-9125-4948-b450-474e225bbc4c', name: 'dc1.1x1', ram: 1024, disk: 40, vcpus: 1)
-  Billing::InstanceFlavor.create(flavor_id: '8f4b7ae1-b8c2-431f-bb0c-362a5ece0381', name: 'dc1.2x4', ram: 4096, disk: 80, vcpus: 2)
-  Billing::InstanceFlavor.create(flavor_id: 'bf6dbcab-f0a5-49d7-b427-0ee09cc5f583', name: 'dc1.2x2', ram: 2048, disk: 80, vcpus: 2)
-  Billing::InstanceFlavor.create(flavor_id: '78d43ae0-7c98-48d2-9adc-90e8f8f6fe99', name: 'dc1.1x0', ram: 512, disk: 10, vcpus: 1)
-  Billing::InstanceFlavor.create(flavor_id: 'b671216b-1c68-4765-b752-0e8e6b6d015f', name: 'dc1.1x2', ram: 2048, disk: 40, vcpus: 1)
-  Billing::InstanceFlavor.create(flavor_id: '7e1d9f77-acdf-41bb-a5e8-572ee153d21f', name: 'dc1.4x8', ram: 8192, disk: 120, vcpus: 4)
+  Billing::InstanceFlavor.create(id: 6, flavor_id: 'ede87d00-b2a7-4b74-acf7-1490a6c29f22', name: 'm1.xlarge', ram: 16384, disk: 80, vcpus: 8)
+  Billing::InstanceFlavor.create(id: 7, flavor_id: 'f0577618-9125-4948-b450-474e225bbc4c', name: 'dc1.1x1', ram: 1024, disk: 40, vcpus: 1)
+  Billing::InstanceFlavor.create(id: 8, flavor_id: '8f4b7ae1-b8c2-431f-bb0c-362a5ece0381', name: 'dc1.2x4', ram: 4096, disk: 80, vcpus: 2)
+  Billing::InstanceFlavor.create(id: 9, flavor_id: 'bf6dbcab-f0a5-49d7-b427-0ee09cc5f583', name: 'dc1.2x2', ram: 2048, disk: 80, vcpus: 2)
+  Billing::InstanceFlavor.create(id: 10, flavor_id: '78d43ae0-7c98-48d2-9adc-90e8f8f6fe99', name: 'dc1.1x0', ram: 512, disk: 10, vcpus: 1)
+  Billing::InstanceFlavor.create(id: 11, flavor_id: 'b671216b-1c68-4765-b752-0e8e6b6d015f', name: 'dc1.1x2', ram: 2048, disk: 40, vcpus: 1)
+  Billing::InstanceFlavor.create(id: 12, flavor_id: '7e1d9f77-acdf-41bb-a5e8-572ee153d21f', name: 'dc1.4x8', ram: 8192, disk: 120, vcpus: 4)
+  Billing::InstanceFlavor.create(id: 13, flavor_id: '05a9e6d1-d29f-4e98-9eab-51c9a6beed44', name: 'dc1.1x2.20', ram: 2048, disk: 20, vcpus: 1)
+  Billing::InstanceFlavor.create(id: 14, flavor_id: '196235bc-7ca5-4085-ac81-7e0242bda3f9', name: 'dc1.2x4.40', ram: 4096, disk: 40, vcpus: 2)
+  Billing::InstanceFlavor.create(id: 15, flavor_id: 'c4b193d2-f331-4250-9b15-bbfde97c462a', name: 'dc1.2x2.40', ram: 2048, disk: 40, vcpus: 2)
+  Billing::InstanceFlavor.create(id: 16, flavor_id: 'af2a80fe-ccad-43df-8cae-6418da948467', name: 'dc1.8x16', ram: 16384, disk: 80, vcpus: 8)
 
-  ["aarch64", "x86_64"].each do |arch|
-    Billing::InstanceFlavor.all.each do |flavor|
-      Billing::Rate.create(flavor_id: flavor.id, arch: arch, rate: 0.5)
-    end
+  rates = [[1, 'x86_64', 0.0089],
+          [2, 'x86_64', 0.0177],
+          [3, 'x86_64', 0.0695],
+          [4, 'x86_64', 0.2817],
+          [5, 'x86_64', 0.1391],
+          [6, 'x86_64', 0.2817],
+          [7, 'x86_64', 0.2817],
+          [8, 'x86_64', 0.0177],
+          [9, 'x86_64', 0.0695],
+          [10, 'x86_64', 0.0353],
+          [11, 'x86_64', 0.0089],
+          [12, 'x86_64', 0.0348],
+          [13, 'x86_64', 0.1391],
+          [14, 'x86_64', 0.1375],
+          [15, 'x86_64', 0.1375],
+          [16, 'x86_64', 0.2817],
+          [17, 'x86_64', 0.2817],
+          [18, 'x86_64', 0.0348],
+          [19, 'x86_64', 0.0177],
+          [20, 'x86_64', 0.0177],
+          [21, 'x86_64', 0.0348],
+          [22, 'x86_64', 0.0695],
+          [23, 'x86_64', 0.275],
+          [24, 'x86_64', 0.0353],
+          [25, 'x86_64', 0.5634],
+          [26, 'x86_64', 0.525],
+          [27, 'x86_64', 0.2817],
+          [28, 'x86_64', 0.0353],
+          [29, 'aarch64', 0.139],
+          [30, 'aarch64', 0.0706],
+          [31, 'aarch64', 0.2782],
+          [32, 'aarch64', 0.55],
+          [33, 'aarch64', 0.0354],
+          [34, 'aarch64', 0.0696],
+          [35, 'x86_64', 0.0354],
+          [37, 'x86_64', 0.0353],
+          [38, 'aarch64', 0.275],
+          [39, 'aarch64', 1.05],
+          [40, 'x86_64', 0.525],
+          [9, 'aarch64', 0.139],
+          [10, 'aarch64', 0.0706],
+          [16, 'aarch64', 0.2782],
+          [23, 'aarch64', 0.55],
+          [8, 'aarch64', 0.0354],
+          [12, 'aarch64', 0.0696],
+          [14, 'aarch64', 0.275]]
+  rates.each do |rate|
+    Billing::Rate.create(flavor_id: rate[0], arch: rate[1], rate: rate[2])
   end
 end
---
+++ test/unit/lib/instances_resize_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class MockServers
+  def get(instance_id); nil; end
+end
+
+class TestInstancesResize < Minitest::Test
+  def setup
+    @from = Time.parse("2015-07-23 08:00:00")
+    @to = Time.parse("2015-07-23 18:00:00")
+    @tenant = Tenant.make!(name: 'datacentred', uuid: 'ed4431814d0a40dc8f10f5ac046267e9')
+    @events = JSON.parse(File.read(File.expand_path("../../../../fixtures/dumps/resize_instance.json", __FILE__)))
+    @servers_mock = OpenStruct.new(servers: MockServers.new)
+    load_instance_flavors
+  end
+
+  def test_calculation_of_cost
+    # Instance starts as 2x2 (billed at 0.0353)
+    # and becomes 8x16 (billed at 0.2817)  
+    Billing.stub(:fetch_samples, @events) do
+      Fog::Compute.stub(:new, @servers_mock) do
+        Billing::Instances.sync!(@from, @to, Billing::Sync.create(started_at: Time.now))
+        instance = Billing::Instance.find_by_instance_id("b246c075-36d8-45ee-a8f5-a44c15158dd9")
+        assert_equal 1, Billing::Instances.cost(instance, @from, @to)
+      end
+    end
+  end
+
+  def teardown
+    DatabaseCleaner.clean
+  end
+
+end
