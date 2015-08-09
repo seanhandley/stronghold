95d7017e3d3b2618fafad279992cd7d5585ddd81
Sean Handley <sean.handley@gmail.com>
2015-08-09 09:40:50 +0100

    Continue on error.

--- app/jobs/card_reverification_job.rb
+++ app/jobs/card_reverification_job.rb
@@ -4,10 +4,15 @@ class CardReverificationJob < ActiveJob::Base
 
   def perform
     Organization.active.select(&:self_service?).select(&:known_to_payment_gateway?).each do |organization|
-      unless stripe_has_valid_source?(organization.stripe_customer_id)
-        organization.has_payment_methods!(false)
-        Mailer.card_reverification_failure(organization).deliver_now
-        Notifications.notify(:account_alert, "#{organization.name} (ref: #{organization.reference}) failed payment reverification. Please investigate ASAP and determine further action. The customer has been notified by email.")
+      begin
+        unless stripe_has_valid_source?(organization.stripe_customer_id)
+          organization.has_payment_methods!(false)
+          Mailer.card_reverification_failure(organization).deliver_now
+          Notifications.notify(:account_alert, "#{organization.name} (ref: #{organization.reference}) failed payment reverification. Please investigate ASAP and determine further action. The customer has been notified by email.")
+        end
+      rescue StandardError => e
+        Honeybadger.notify(e)
+        next
       end
     end
   end
