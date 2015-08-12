7bbd28b890cc979f344104091cf35df0f53fc761
Sean Handley <sean.handley@gmail.com>
2015-08-12 14:18:37 +0100

    Retrieve maintenance and incident lists

--- lib/status_io.rb
+++ lib/status_io.rb
@@ -13,6 +13,28 @@ module StatusIO
     end
   end
 
+  def self.list_items(key, filter)
+    resp = conn.get do |req|
+      req.url "/v2/#{key}/list/#{Rails.application.secrets.status_io_page_id}"
+      req.headers['Content-Type'] = 'application/json'
+      req.headers['API-ID'] = Rails.application.secrets.status_io_id
+      req.headers['API-KEY'] = Rails.application.secrets.status_io_key
+    end
+    JSON.parse(resp.body)['result'][filter]
+  end
+
+  def self.active_incidents
+    list_items('incident', 'active_incidents')
+  end
+
+  def self.active_maintenances
+    list_items('maintenance', 'active_maintenances')
+  end
+
+  def self.upcoming_maintenances
+    list_items('maintenance', 'upcoming_maintenances')
+  end
+
   private
 
   def self.conn
