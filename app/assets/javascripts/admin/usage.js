92889fff915675fadd52bb333e419a6a5c2931b5
Sean Handley <sean.handley@gmail.com>
2015-08-14 19:41:38 +0100

    Screw coffee script

---
+++ app/assets/javascripts/admin/usage.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("#project").chained("#organization");
+  $('a.toggle_line_entry').click(function(e) {
+    e.preventDefault();
+    if ($(this).text().charAt(0) == "+") {
+      $(this).text("-" + $(this).text().substring(1));
+      $("." + $(this).data('id')).removeClass('hide');
+    } else {
+      $(this).text("+" + $(this).text().substring(1));
+      $("." + $(this).data('id')).addClass('hide');
+    }
+  });
+});
