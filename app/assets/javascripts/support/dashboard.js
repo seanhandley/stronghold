c42300d8d09847a1ced61c849c6e27c1a948521b
Sean Handley <sean.handley@gmail.com>
2015-08-20 16:00:52 +0100

    Spit and polish.

--- app/assets/javascripts/support/dashboard.js
+++ app/assets/javascripts/support/dashboard.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
   $('#regenerate-ceph-credentials').click(function(e) {
     e.preventDefault();
-    $('#loading-overlay').removeClass('hide');
     if (confirm("This can't be undone and any applications/users that use these credentials will be locked out until provided with the new credentials. Are you sure?")) {
+      $('#loading-overlay').removeClass('hide');
       $.post('/regenerate_ceph_credentials', function(data) {
         if(data.success) {
           $('#loading-overlay').addClass('hide');
           $('#ceph-access-key').text(data.credentials.access);
-          $('#ceph-secret-key').text($("<em>Click To Reveal</em>"));
+          $('#ceph-secret-key').empty();
+          $('#ceph-secret-key').append("<em>Click To Reveal</em>");
           $('#ceph-secret-key').data('reveal', data.credentials.secret);
         } else {
           alert(data.message);
