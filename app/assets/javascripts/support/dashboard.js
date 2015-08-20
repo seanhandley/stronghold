c739f022e5dcd43fcd15396a1fcada47ac862745
Sean Handley <sean.handley@gmail.com>
2015-08-20 15:51:22 +0100

    Polish.

--- app/assets/javascripts/support/dashboard.js
+++ app/assets/javascripts/support/dashboard.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
   $('#regenerate-ceph-credentials').click(function(e) {
     e.preventDefault();
+    $('#loading-overlay').removeClass('hide');
     if (confirm("This can't be undone and any applications/users that use these credentials will be locked out until provided with the new credentials. Are you sure?")) {
       $.post('/regenerate_ceph_credentials', function(data) {
         if(data.success) {
-          console.log(data.credentials)
+          $('#loading-overlay').addClass('hide');
           $('#ceph-access-key').text(data.credentials.access);
-          $('#ceph-secret-key').text("<em>Click To Reveal</em>");
+          $('#ceph-secret-key').text($("<em>Click To Reveal</em>"));
           $('#ceph-secret-key').data('reveal', data.credentials.secret);
         } else {
           alert(data.message);
