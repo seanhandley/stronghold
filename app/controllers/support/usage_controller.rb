7c04b20c20b7bcf1e1f668c7e3d561479095e68a
Sean Handley <sean.handley@gmail.com>
2015-08-21 15:13:47 +0100

    Helps if you compare equivalent types.

--- app/controllers/support/usage_controller.rb
+++ app/controllers/support/usage_controller.rb
@@ -10,7 +10,7 @@ class Support::UsageController < SupportBaseController
 
   def index
     @usage_decorator = UsageDecorator.new(current_user.organization)
-    if params[:year] && params[:month] && !((params[:year] == Time.now.year) && (params[:month] == Time.now.month))
+    if params[:year] && params[:month] && !((params[:year].to_i == Time.now.year) && (params[:month].to_i == Time.now.month))
       @usage = @usage_decorator.usage_data(from_date: @from_date, to_date: @to_date)
     else
       @usage = @usage_decorator.latest_usage_data
