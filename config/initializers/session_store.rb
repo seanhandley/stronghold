7a0db00adff4efc3a3fae91c293c9c4ac469941d
Sean Handley <sean.handley@gmail.com>
2015-09-01 13:32:11 +0100

    Set the domain for each environment.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
+APP_DOMAIN = 'localhost' unless defined?(APP_DOMAIN)
+
 Rails.application.config.session_store ActionDispatch::Session::MemCacheStore, :expire_after => 1.hour, :key => '_stronghold_session', domain: APP_DOMAIN
