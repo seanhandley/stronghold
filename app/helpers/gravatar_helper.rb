d6f41f9fa71dab14ffe84a5227e3206eab2234c5
Sean Handley <sean.handley@gmail.com>
2015-09-01 15:14:23 +0100

    Because it doesn't cache the image itself.

--- app/helpers/gravatar_helper.rb
+++
@@ -1,7 +0,0 @@
-module GravatarHelper
-  def cached_gravatar_image(key, params={})
-    Rails.cache.fetch("gravatar_#{key}", expires_in: 20.minutes) do
-      gravatar_image_tag(key, params)
-    end
-  end
-end
