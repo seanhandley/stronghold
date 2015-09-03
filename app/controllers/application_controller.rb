9a92d0b6b97f04b4a3685aae306f3c6d8454cc15
Sean Handley <sean.handley@gmail.com>
2015-09-03 10:28:27 +0100

    Lazy load.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
 
   before_filter :device_cookie
 
