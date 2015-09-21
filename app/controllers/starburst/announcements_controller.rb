23680cc5c3a3a01ecfc2a47d6f90d25e7914721b
Sean Handley <sean.handley@gmail.com>
2015-09-21 10:33:15 +0100

    Use the whole controller.

--- app/controllers/starburst/announcements_controller.rb
+++ app/controllers/starburst/announcements_controller.rb
@@ -1,5 +1,21 @@
+require_dependency 'starburst/application_controller'
+
 module Starburst
   class AnnouncementsController < ApplicationController
+
     skip_before_action :verify_authenticity_token
+
+    def mark_as_read
+      announcement = Announcement.find(params[:id].to_i)
+      if respond_to?(Starburst.current_user_method) && send(Starburst.current_user_method) && announcement
+        if AnnouncementView.where(user_id: send(Starburst.current_user_method).id, announcement_id: announcement.id).first_or_create(user_id: send(Starburst.current_user_method).id, announcement_id: announcement.id)
+          render :json => :ok
+        else
+          render json: nil, :status => :unprocessable_entity
+        end
+      else
+          render json: nil, :status => :unprocessable_entity
+      end
+    end
   end
 end
