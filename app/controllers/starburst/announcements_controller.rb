2e416148cce6f25c654cd5955c9f6381b37bf62e
Sean Handley <sean.handley@gmail.com>
2015-09-21 10:41:07 +0100

    Debug.

--- app/controllers/starburst/announcements_controller.rb
+++ app/controllers/starburst/announcements_controller.rb
@@ -11,9 +11,19 @@ module Starburst
         if AnnouncementView.where(user_id: send(Starburst.current_user_method).id, announcement_id: announcement.id).first_or_create(user_id: send(Starburst.current_user_method).id, announcement_id: announcement.id)
           render :json => :ok
         else
+          Rails.logger.error "*" * 10
+          Rails.logger.error "Foo"
+          Rails.logger.error announcement.inspect
+          Rails.logger.error respond_to?(Starburst.current_user_method)
+          Rails.logger.error send(Starburst.current_user_method)
           render json: nil, :status => :unprocessable_entity
         end
       else
+          Rails.logger.error "*" * 10
+          Rails.logger.error "Bar"
+          Rails.logger.error announcement.inspect
+          Rails.logger.error respond_to?(Starburst.current_user_method)
+          Rails.logger.error send(Starburst.current_user_method)
           render json: nil, :status => :unprocessable_entity
       end
     end
