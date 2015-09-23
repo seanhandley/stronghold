004c9d2e433de827d11d2f3446260e84b2800851
Sean Handley <sean.handley@gmail.com>
2015-09-23 12:55:30 +0100

    Plonker.

--- lib/billing/instances.rb
+++ lib/billing/instances.rb
@@ -157,7 +157,7 @@ module Billing
           end
         end
       else
-        if previous_state && billable?(previous_state.state)
+        if previous_state && billable?(previous_state.state) && instance.active?
           return (to - from).round
         else
           return 0
