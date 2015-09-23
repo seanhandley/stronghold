1ec3c1156f3e3f0ad5c737b86b09904117063a96
Sean Handley <sean.handley@gmail.com>
2015-09-23 13:50:31 +0100

    Rethink how we show number of servers in flight.

--- app/controllers/support/dashboard_controller.rb
+++ app/controllers/support/dashboard_controller.rb
@@ -22,9 +22,13 @@ class Support::DashboardController < SupportBaseController
   private
 
   def instance_count
-    Rails.cache.fetch("instance_count_#{current_user.organization.primary_tenant.id}", expires_in: 5.minutes) do
-      OpenStack::Instance.all.count rescue 0
-    end  
+    live_servers.select{|s| current_user.organization.tenants.map(&:uuid).include?(['tenant_id'])}.count
+  end
+
+  def live_servers
+    Rails.cache.fetch("live_servers", expires_in: 5.minutes) do
+      Fog::Compute.new(OPENSTACK_ARGS).list_servers_detail(all_tenants: true).body['servers']
+    end 
   end
 
   def object_usage
