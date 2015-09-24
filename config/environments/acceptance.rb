af52f94f573ff0cf7898128ca837d8e486029824
Sean Handley <sean.handley@gmail.com>
2015-09-24 13:03:57 +0100

    Use forgery protection in acceptance.

--- config/environments/acceptance.rb
+++ config/environments/acceptance.rb
@@ -42,9 +42,8 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-
   # Disable request forgery protection in test environment.
-  config.action_controller.allow_forgery_protection = false
+  config.action_controller.allow_forgery_protection = true
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
