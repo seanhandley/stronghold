5b1b879c1badd07c43d026c1be20c150dc0d93a5
Sean Handley <sean.handley@gmail.com>
2015-09-29 08:05:01 +0100

    Only run one usage job at a time.

--- app/jobs/usage_job.rb
+++ app/jobs/usage_job.rb
@@ -1,9 +1,16 @@
 class UsageJob < ActiveJob::Base
   queue_as :default
 
-  # sidekiq_options unique: true
-
   def perform
-    Billing.sync!
+    Billing.sync! unless already_running?
+  end
+
+  private
+
+  def already_running?
+    Sidekiq::Workers.new.each do |process_id, thread_id, work|
+      return true if work['payload']['wrapped'] == 'UsageJob'
+    end
+    false
   end
 end
