98376913e370d170153a194b86afe18e9d85a7d1
Sean Handley <sean.handley@gmail.com>
2015-09-29 08:57:59 +0100

    Make cache jobs unique.

--- app/jobs/usage_cache_refresh_job.rb
+++ app/jobs/usage_cache_refresh_job.rb
@@ -2,8 +2,9 @@ class UsageCacheRefreshJob < ActiveJob::Base
   queue_as :usage_cache
 
   def perform(organization=nil)
-    return warm_cache(organization) if organization
-
+    @organization = organization
+    return warm_cache(organization) if @organization && !already_running?
+    
     dispersal_time = 600
     spacing = dispersal_time / Organization.active.count
     Organization.active.each_with_index do |organization, i|
@@ -17,5 +18,18 @@ class UsageCacheRefreshJob < ActiveJob::Base
   def warm_cache(organization)
     ud = UsageDecorator.new(organization)
     ud.usage_data(from_date: Time.now.beginning_of_month, to_date: Time.now)
+  end
+
+  def already_running?
+    usage_cache_refresh_job_count = Sidekiq::Workers.new.select do |process_id, thread_id, work|
+      work['payload']['wrapped'] == 'UsageCacheRefreshJob' &&
+      work['payload']['args'].count > 0 &&
+      org_id_from_global(work['payload']['args'].first) == @organization.id
+    end.count
+    usage_cache_refresh_job_count > 1
+  end
+
+  def org_id_from_global(global)
+    global["_aj_globalid"].split('/').last.to_i
   end
 end
