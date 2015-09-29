cb48002607eac202ce4c554efd35eeb6f4021802
Sean Handley <sean.handley@gmail.com>
2015-09-29 10:01:00 +0100

    Login before checking CSRF.

--- app/controllers/authorized_controller.rb
+++ app/controllers/authorized_controller.rb
@@ -9,6 +9,10 @@ class AuthorizedController < ApplicationController
 
   check_authorization
 
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
   def current_ability
     @current_ability ||= User::Ability.new(current_user)
   end
