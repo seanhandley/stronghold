4e37a2c2350fb55148a10851e90f3c7347d43861
Sean Handley <sean.handley@gmail.com>
2015-10-02 18:48:17 +0100

    More exploratory work.

--- app/models/usage_data_set.rb
+++ app/models/usage_data_set.rb
@@ -1,16 +1,15 @@
 class UsageDataSet
-  attr_reader :organization, :usage_data, :from_date, :to_date
+  attr_reader :organization, :usage_data, :month, :year
 
   def initialize(organization, year, month)
     @organization = organization
-    @from_date = from_date
-    @to_date = to_date
+    @month = month
+    @year = year
   end
 
   def usage_data
-    Rails.cache.fetch(key, expires_in: 6.months) do
-      build_data_from_fresh
-    end
+    this_month = Rails.cache.read(org_index_key)[org_month_key]
+    merge_usage_data(this_month.collect{|entry_key| Rails.cache.read(entry_key)})
   end
 
   private
@@ -23,41 +22,96 @@ class UsageDataSet
     "org#{organization.id}_usage_#{new_uuid}"
   end
 
+  def org_month_key
+    "#{year}-#{month}"
+  end
+
   def org_index
     Rails.cache.fetch(org_index_key, expires_in: 6.months) do
-      []
+      {}
     end
   end
 
   def update_org_index(new_key)
     index = Rails.cache.read(org_index_key)
-    Rails.cache.write(org_index_key, index << new_key, expires_in: 6.months)
+    this_months_entry = index[org_month_key]
+    this_months_entry = this_months_entry.to_a << new_key
+    Rails.cache.write(org_index_key, this_months_entry, expires_in: 6.months)
   end
 
-  def build_data_from_fresh
-    new_uuid = SecureRandom.hex
-    usage_data = organization.tenants.inject({}) do |acc, tenant|
-      acc[tenant] = {
-        instance_results: Billing::Instances.usage(tenant.uuid, from_date, to_date),
-        volume_results: Billing::Volumes.usage(tenant.uuid, from_date, to_date),
-        image_results: Billing::Images.usage(tenant.uuid, from_date, to_date),
-        floating_ip_results: Billing::FloatingIps.usage(tenant.uuid, from_date, to_date),
-        ip_quota_results: Billing::IpQuotas.usage(tenant.uuid, from_date, to_date),
-        external_gateway_results: Billing::ExternalGateways.usage(tenant.uuid, from_date, to_date),
-        object_storage_results: Billing::StorageObjects.usage(tenant.uuid, from_date, to_date)
-      }
-      acc
-    end
-    Rails.cache.write(org_usage_key(new_uuid), expires_in: 6.months)
-      {
-        usage_data: usage_data,
-        organization: organization,
-        from_date: from_date,
-        to_date: to_date,
-        uuid: uuid
-      }
-    end
-    update_org_index(org_usage_key(new_uuid))
+  def merge_usage_data(entries)
+    int = nil
+    string = nil
+    datetime = nil
+    float = nil
+    ip_quota_objects = nil
+    {
+      instance_results: [
+        {
+          billable_seconds: int,
+          uuid: string,
+          name: string,
+          tenant_id: string,
+          first_booted_at: datetime,
+          latest_state: string,
+          terminated_at: datetime,
+          rate: float,
+          billable_hours: int,
+          cost: float,
+          arch: string,
+          flavor: {
+            flavor_id: string,
+            name: string,
+            vcpus_count: int,
+            ram_mb: int,
+            root_disk_gb: int,
+            rate: float
+          },
+          image: {
+            image_id: string,
+            name: string
+          }
+        }
+      ],
+      volume_results: [
+        { 
+          terabyte_hours: float,
+          cost: float,
+          id: string,
+          created_at: datetime,
+          deleted_at: datetime,
+          latest_size: float,
+          name: string
+        }
+      ],
+      image_results: [
+        {
+          terabyte_hours: float,
+          cost: float,
+          id: string,
+          created_at: datetime,
+          deleted_at: datetime,
+          latest_size: float,
+          name: string
+        }
+      ],
+      floating_ip_results: [
+        {
+          billable_seconds: int,
+          address: string,
+          quota: int
+        }
+      ],
+      ip_quota_results: [ip_quota_objects],
+      external_gateway_results: [
+        {
+          billable_seconds: int,
+          id: string,
+          name: string
+        }
+      ],
+      object_storage_results: float
+    }
   end
 
   def timestamp_format
