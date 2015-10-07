45a0deafa4d68d005ffc58b0e82be88062185a96
Sean Handley <sean.handley@gmail.com>
2015-10-07 20:22:45 +0100

    Revert "Try GET instead."
    This reverts commit 4e726515454ee34061fed0e65359d3af862c4e06.

--- config/routes.rb
+++ config/routes.rb
@@ -69,7 +69,7 @@ Rails.application.routes.draw do
   post 'signup/:token', :controller => 'signups', :action => 'update', :as => 'signup_complete'
   get 'signup', :controller => 'signups', :action => 'new', :as => 'new_signup'
   post 'signup', :controller => 'signups', :action => 'create', :as => 'create_signup'
-  get 'sign_out', :controller => 'sessions', :action => 'destroy', :as => 'signout'
+  post 'sign_out', :controller => 'sessions', :action => 'destroy', :as => 'signout'
   post 'wait_list', :controller => 'wait_list_entries', :action => 'create', :as => 'wait_list'
   post 'precheck', :controller => 'ajax', :action => 'precheck'
   post 'cc_submit', :controller => 'ajax', :action => 'cc_submit'
