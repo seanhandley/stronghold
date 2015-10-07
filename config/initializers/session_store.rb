d7ed6ccdd023db1f1b148964420246958ef36cec
Sean Handley <sean.handley@gmail.com>
2015-10-07 20:12:23 +0100

    Nah.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 keys = {:expire_after => 1.hour, :key => '_stronghold_session'}
-if defined?(APP_DOMAIN)
-  keys.merge!(domain: APP_DOMAIN)
-end
+# if defined?(APP_DOMAIN)
+#   keys.merge!(domain: APP_DOMAIN)
+# end
 
 # If we want to use memcached
 # Rails.application.config.session_store ActionDispatch::Session::MemCacheStore, keys
