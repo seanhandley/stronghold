015860598e0a4a2010fa45fda93f0bce1fca273d
Sean Handley <sean.handley@gmail.com>
2015-10-14 13:08:42 +0100

    Include these in the model.

--- app/models/reports/usage_report.rb
+++ app/models/reports/usage_report.rb
@@ -37,29 +37,29 @@ module Reports
         }
       end.sort{|x,y| y[:spend] <=> x[:spend]}.take(30)
     end
-  end
 
-  private
+    private
 
-  def organizations
-    Organization.active.includes(:tenants).select(&:cloud?).reject(&:test_account)
-  end
-
-  def get_usage(klass, tenants, from, to)
-    tenants.collect do |tenant|
-      klass.usage(tenant.uuid, from, to)
-    end.flatten
-  end
-
-  def instance_usage_aggregated_data(instances)
-    vcpu_hours, ram_tb_hours, instances_disk_tb_hours  = [], [], []
-    instances.each do |instance|
-      billable_hours = instance[:billable_hours]
-      vcpu_hours              << (billable_hours * instance[:flavor][:vcpus_count])
-      ram_tb_hours            << (billable_hours * (instance[:flavor][:ram_mb] / 1_048_576.0))
-      instances_disk_tb_hours << (billable_hours * (instance[:flavor][:root_disk_gb] / 1024.0))
+    def organizations
+      Organization.active.includes(:tenants).select(&:cloud?).reject(&:test_account)
     end
-    
-    [vcpu_seconds.sum, ram_tb_seconds.sum, instances_disk_tb_hours.sum]
+
+    def get_usage(klass, tenants, from, to)
+      tenants.collect do |tenant|
+        klass.usage(tenant.uuid, from, to)
+      end.flatten
+    end
+
+    def instance_usage_aggregated_data(instances)
+      vcpu_hours, ram_tb_hours, instances_disk_tb_hours  = [], [], []
+      instances.each do |instance|
+        billable_hours = instance[:billable_hours]
+        vcpu_hours              << (billable_hours * instance[:flavor][:vcpus_count])
+        ram_tb_hours            << (billable_hours * (instance[:flavor][:ram_mb] / 1_048_576.0))
+        instances_disk_tb_hours << (billable_hours * (instance[:flavor][:root_disk_gb] / 1024.0))
+      end
+      
+      [vcpu_seconds.sum, ram_tb_seconds.sum, instances_disk_tb_hours.sum]
+    end
   end
 end
