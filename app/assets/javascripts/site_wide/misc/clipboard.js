0f99821dd88a624cd613655eda0cb3d0c091fc46
Sean Handley <sean.handley@gmail.com>
2015-10-27 20:38:21 +0000

    Use clipboard.js from rails assets.

---
+++ app/assets/javascripts/site_wide/misc/clipboard.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  var clipboard = new Clipboard('.btn-copy');
+
+  clipboard.on('success', function(e) {
+      $(e.trigger).data('title', "Copied!");
+      $(e.trigger).tooltip('show');
+      setTimeout(function() {
+        $('.btn-copy').tooltip('hide')
+      }, 1600);
+
+      e.clearSelection();
+  });
+
+  clipboard.on('error', function(e) {
+    $(e.trigger).data('title', "Press Ctrl+C to copy");
+    $(e.trigger).tooltip('show');
+    setTimeout(function() {
+      $('.btn-copy').tooltip('hide')
+    }, 1600);
+  });
+});
