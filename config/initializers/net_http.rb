c73e170d4e89c88283dcb4022baa84c7df1efd3b
Sean Handley <sean.handley@gmail.com>
2015-10-28 12:00:58 +0000

    Set some sane defaults for libs using net http.

---
+++ config/initializers/net_http.rb
@@ -0,0 +1,14 @@
+require 'net/http'
+module Net
+  class HTTP < Protocol
+    alias default_timeout_initializer initialize
+    def initialize(address, port = nil)
+      default_timeout_initializer(address, port)
+      #@keep_alive_timeout = 2
+      @open_timeout = 2
+      @read_timeout = 5
+      @continue_timeout = 5
+      @ssl_timeout = 2
+    end
+  end
+end
