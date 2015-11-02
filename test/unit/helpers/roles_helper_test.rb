72bb716cd7db3f16be5800c2033f1aa907bc54f3
Sean Handley <sean.handley@gmail.com>
2015-11-02 07:42:48 +0000

    More helper tests.

--- test/unit/helpers/roles_helper_test.rb
+++ test/unit/helpers/roles_helper_test.rb
@@ -1,16 +1,48 @@
 require 'test_helper'
 
 class TestModel
+  include ActionView::Helpers::FormOptionsHelper
   include RolesHelper
+
+  def params ; end
 end
 
 class RolesHelperTest < Minitest::Test
   def setup
     @model = TestModel.new
+    @user = User.make!
+    @user2 = User.make!(organization: @user.organization)
+    @role = Role.make!(name: 'Special Powers', organization: @user.organization)
+    @user.roles << @role
+    @user.save
   end
 
-  def test_foo
-    flunk
+  def test_list_of_roles
+    assert_equal 'Special Powers', @model.list_of_roles(@user)
+  end
+
+  def test_roles_for_select
+    assert_equal "<option value=\"#{@role.id}\">Special Powers</option>", @model.roles_for_select(@user.organization)
+  end
+
+  def test_users_for_select
+    assert_equal "<option value=\"#{@user2.id}\">#{@user2.name}</option>", @model.users_for_select(@role)
+  end
+
+  def test_active_tab
+    @model.stub(:params, {tab: 'foo'}) do
+      assert_equal '', @model.active_tab?('bar')
+      assert_equal 'active', @model.active_tab?('foo')
+    end
+
+    @model.stub(:params, {}) do
+      assert_equal '', @model.active_tab?('bar')
+      assert_equal 'active', @model.active_tab?('users')
+    end
+  end
+
+  def test_invite_status_label
+    
   end
 
 end
