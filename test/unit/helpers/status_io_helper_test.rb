459617fd9d16c3412682b9a072c09e03f64e9bdb
Sean Handley <sean.handley@gmail.com>
2015-11-02 09:57:19 +0000

    More tests.

--- test/unit/helpers/status_io_helper_test.rb
+++ test/unit/helpers/status_io_helper_test.rb
@@ -9,8 +9,12 @@ class StatusIoHelperTest < Minitest::Test
     @model = TestModel.new
   end
 
-  def test_foo
-    flunk
+  def test_status_io_url
+    dummy = OpenStruct.new(secrets: OpenStruct.new(status_io_page_id: 'foo'))
+    Rails.stub(:application, dummy) do
+      assert_equal "http://status.datacentred.io/pages/baz/foo/bar",
+                   @model.status_io_url('baz', 'bar')
+    end
   end
 
 end
