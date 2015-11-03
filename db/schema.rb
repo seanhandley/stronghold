64f763e9a42bdd6c4069311a4c85cc350fe5b516
Sean Handley <sean.handley@gmail.com>
2015-11-03 13:58:54 +0000

    Update schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151019083630) do
+ActiveRecord::Schema.define(version: 20151103122715) do
 
   create_table "audits", force: :cascade do |t|
     t.string   "auditable_id",    limit: 255
