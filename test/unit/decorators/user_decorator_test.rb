4f07884fa5942ee12472047fc47a9b368819244b
Sean Handley <sean.handley@gmail.com>
2015-11-03 11:03:51 +0000

    Decorator test.

---
+++ test/unit/decorators/user_decorator_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class UserDecoratorTest < CleanTest
+  def setup
+    @user = UserDecorator.new(User.make!)
+  end
+
+  def test_as_sirportly_data
+    expected = {
+      :reference => @user.unique_id,
+      :contact_methods => {
+        :email => [@user.email]
+      },
+      :first_name => @user.first_name,
+      :last_name  => @user.last_name,
+      :company    => @user.organization.reference,
+      :timezone   => @user.organization.time_zone
+    }
+    assert_equal expected, @user.as_sirportly_data
+  end
+
+end
