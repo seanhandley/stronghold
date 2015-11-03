d147355116f7fdd2c78d04645d52fb6a142389bc
Sean Handley <sean.handley@gmail.com>
2015-11-03 11:03:45 +0000

    Missing tests.

--- test/unit/helpers/usage_helper_test.rb
+++ test/unit/helpers/usage_helper_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class TestModel
   include UsageHelper
+  include Rails.application.routes.url_helpers
 
   def current_user
     @user ||= User.make(organization: Organization.make(created_at: signed_up))
@@ -14,9 +15,16 @@ class TestModel
   def signed_up
     Time.parse('2014-01-01')
   end
+
+  def params
+    {
+      month: Date.today.month,
+      year: Date.today.year
+    }
+  end
 end
 
-class UsageHelperTest < Minitest::Test
+class UsageHelperTest < CleanTest
   def setup
     @model = TestModel.new
   end
@@ -68,24 +76,45 @@ class UsageHelperTest < Minitest::Test
   end
 
   def test_usages_for_select
-    skip
+    Timecop.freeze(Time.parse('2014-03-02')) do
+      options = "<option selected=\"selected\" value=\"/account/usage?month=3&amp;year=2014\">March 2014</option>\n<option value=\"/account/usage?month=2&amp;year=2014\">February 2014</option>\n<option value=\"/account/usage?month=1&amp;year=2014\">January 2014</option>"
+      assert_equal options, @model.usages_for_select(@model.current_organization)
+    end
+
+    Timecop.freeze(Time.parse('2014-01-31')) do
+      options = "<option selected=\"selected\" value=\"/account/usage?month=1&amp;year=2014\">January 2014</option>"
+      assert_equal options, @model.usages_for_select(@model.current_organization)
+    end
   end
 
   def test_billing_range
-    skip
+    range = @model.billing_range(@model.current_organization)
+    assert_equal @model.current_organization.created_at.beginning_of_month,
+                 range.last
+    assert_equal Date.today.end_of_month, range.first
+    Timecop.freeze(Time.now + 2.years + 3.months) do
+      range = @model.billing_range(@model.current_organization)
+      assert_equal @model.current_organization.created_at.beginning_of_month,
+                 range.last
+      assert_equal Date.today.end_of_month, range.first
+    end
   end
 
   def test_architecture_human_name
-    
+    assert_equal '-', @model.architecture_human_name(nil)
+    assert_equal '-', @model.architecture_human_name('')
+    assert_equal 'ARM', @model.architecture_human_name('aarch64')
+    assert_equal 'ARM', @model.architecture_human_name('aarch64'.upcase)
+    assert_equal 'Intel x86', @model.architecture_human_name('x86_64')
+    assert_equal 'Intel x86', @model.architecture_human_name('x86_64'.upcase)
+    assert_equal 'foom!', @model.architecture_human_name('foom!')
   end
 
   def test_state_with_icon
-    
+    assert_equal "<i class='fa fa-play text-success'></i> Active", @model.state_with_icon('active')
+    assert_equal "<i class='fa fa-pause'></i> Stopped", @model.state_with_icon('stopped')
+    assert_equal "<i class='fa fa-eject text-danger'></i> Terminated", @model.state_with_icon('terminated')
+    assert_equal 'foom!', @model.state_with_icon('foom!')
   end
-
-  def teardown
-    DatabaseCleaner.clean  
-  end  
-
 
 end
