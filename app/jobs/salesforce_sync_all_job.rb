cacc0ac646f9ad6e6ddb417101842ee6129b3304
Sean Handley <sean.handley@gmail.com>
2015-11-10 11:45:13 +0000

    Warm up the caches.

--- app/jobs/salesforce_sync_all_job.rb
+++ app/jobs/salesforce_sync_all_job.rb
@@ -2,6 +2,9 @@ class SalesforceSyncAllJob < ActiveJob::Base
   queue_as :default
 
   def perform
+    # Warm up the cache before the threads have at it
+    OpenStackConnection.usage((Time.now - 1.week).beginning_of_week, (Time.now - 1.week).end_of_week)
+    OpenStackConnection.usage((Time.now - 1.month).beginning_of_month, (Time.now - 1.month).end_of_month)
     Organization.active.each do |organization|
       organization.update_salesforce_object
     end
