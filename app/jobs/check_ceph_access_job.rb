98b1b1790830d91c69fc115e975d9fea1c773b1e
Sean Handley <sean.handley@gmail.com>
2015-11-13 14:33:22 +0000

    Just do the primary.

--- app/jobs/check_ceph_access_job.rb
+++ app/jobs/check_ceph_access_job.rb
@@ -3,14 +3,12 @@ class CheckCephAccessJob < ActiveJob::Base
 
   def perform(user)
     if User::Ability.new(user).can?(:read, :storage)
-      user.organization.tenants.each do |tenant|
-        begin
-          Ceph::UserKey.create 'uid' => tenant.uuid,
-                               'access-key' => user.ec2_credentials['access'],
-                               'secret-key' => user.ec2_credentials['secret'] if user.ec2_credentials
-        rescue Net::HTTPError => e
-          Honeybadger.notify(e) unless e.message.include? 'BucketAlreadyExists'
-        end
+      begin
+        Ceph::UserKey.create 'uid' => user.organization.primary_tenant.uuid,
+                             'access-key' => user.ec2_credentials['access'],
+                             'secret-key' => user.ec2_credentials['secret'] if user.ec2_credentials
+      rescue Net::HTTPError => e
+        Honeybadger.notify(e) unless e.message.include? 'BucketAlreadyExists'
       end
     else
       user.organization.tenants.each do |tenant|
