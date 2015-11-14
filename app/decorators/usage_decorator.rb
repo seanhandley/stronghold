afebba169996448002278998f3609b3508882bc7
Sean Handley <sean.handley@gmail.com>
2015-11-14 12:03:55 +0000

    Don't collect these.

--- app/decorators/usage_decorator.rb
+++ app/decorators/usage_decorator.rb
@@ -19,7 +19,7 @@ class UsageDecorator < ApplicationDecorator
           instance_results: Billing::Instances.usage(tenant.uuid, from_date, to_date),
           volume_results: Billing::Volumes.usage(tenant.uuid, from_date, to_date),
           image_results: Billing::Images.usage(tenant.uuid, from_date, to_date),
-          floating_ip_results: Billing::FloatingIps.usage(tenant.uuid, from_date, to_date),
+          # floating_ip_results: Billing::FloatingIps.usage(tenant.uuid, from_date, to_date),
           ip_quota_results: Billing::IpQuotas.usage(tenant.uuid, from_date, to_date),
           external_gateway_results: Billing::ExternalGateways.usage(tenant.uuid, from_date, to_date),
           object_storage_results: Billing::StorageObjects.usage(tenant.uuid, from_date, to_date)
