164bdd4881ff0945e4e76aa8943e5a0ec47226df
Sean Handley <sean.handley@gmail.com>
2015-12-01 16:36:02 +0000

    Change label.

--- app/helpers/abbreviation_helper.rb
+++ app/helpers/abbreviation_helper.rb
@@ -2,6 +2,6 @@ module AbbreviationHelper
   def fix_abbreviation_case(s)
     s.gsub('Ip','IP').gsub('Isp', 'ISP').
       gsub('Rams', 'MB RAM').gsub('Floatingip', 'Floating IP').
-      gsub('Gigabytes', 'GB')
+      gsub('Gigabytes', 'GB').gsub('Pools', 'LB Pools')
   end
 end
