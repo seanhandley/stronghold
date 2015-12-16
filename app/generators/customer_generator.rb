6da068e0adf713031ebc31889e4d42df1a8260a5
Sean Handley <sean.handley@gmail.com>
2015-12-16 17:11:28 +0000

    Fix tests.

--- app/generators/customer_generator.rb
+++ app/generators/customer_generator.rb
@@ -70,16 +70,18 @@ class CustomerGenerator
   end
 
   def create_default_network(organization)
-    organization.tenants.collect(&:uuid).each do |tenant_id|
-      n = OpenStackConnection.network.networks.create name: 'default', tenant_id: tenant_id
-      s = OpenStackConnection.network.subnets.create name: 'default', cidr: '192.168.0.0/24',
-                                   network_id: n.id, ip_version: 4, dns_nameservers: ['8.8.8.8', '8.8.4.4'],
-                                   tenant_id: tenant_id
-      external_network = OpenStackConnection.network.networks.select{|n| n.router_external == true }.first
-      r = OpenStackConnection.network.routers.create name: 'default', tenant_id: tenant_id,
-                                   external_gateway_info: external_network.id
-      OpenStackConnection.network.add_router_interface(r.id, s.id)
+    if Rails.env.production?
+      organization.tenants.collect(&:uuid).each do |tenant_id|
+        n = OpenStackConnection.network.networks.create name: 'default', tenant_id: tenant_id
+        s = OpenStackConnection.network.subnets.create name: 'default', cidr: '192.168.0.0/24',
+                                     network_id: n.id, ip_version: 4, dns_nameservers: ['8.8.8.8', '8.8.4.4'],
+                                     tenant_id: tenant_id
+        external_network = OpenStackConnection.network.networks.select{|n| n.router_external == true }.first
+        r = OpenStackConnection.network.routers.create name: 'default', tenant_id: tenant_id,
+                                     external_gateway_info: external_network.id
+        OpenStackConnection.network.add_router_interface(r.id, s.id)
 
+      end
     end
   end
 end
