79183d144b718b24e8472ac67aa611fec307c542
Sean Handley <sean.handley@gmail.com>
2016-01-13 18:55:00 +0000

    Change messages.

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -7,9 +7,9 @@ class StripeController < ApplicationController
 
     case params["type"]
     when "invoice.payment_succeeded"
-      Notifications.notify(:stripe_success, "Invoice #{stripe_info[:invoice_id]}: Customer #{stripe_info[:customer_description]} successfully billed for £#{stripe_info[:total]}.")
+      Notifications.notify(:stripe_success, "Invoice #{stripe_info[:invoice_id]} for £#{stripe_info[:total]} successfully chared to #{stripe_info[:customer_description]}.")
     when "invoice.payment_failed"
-      Notifications.notify(:stripe_fail, "Invoice #{stripe_info[:invoice_id]}: Payment of £#{stripe_info[:total]} for #{stripe_info[:customer_description]} has failed.")
+      Notifications.notify(:stripe_fail, "Invoice #{stripe_info[:invoice_id]} for £#{stripe_info[:total]} could not be charged to #{stripe_info[:customer_description]}.")
     end
 
     render status: 200, json: nil
