a4458c718c655f6ddcd37f293dd273cd4aa35d61
Sean Handley <sean.handley@gmail.com>
2016-01-14 08:49:29 +0000

    Don't notify Hipchat.

--- config/initializers/hipchat.rb
+++
@@ -1,4 +0,0 @@
-settings = YAML.load_file("#{Rails.root}/config/hipchat.yml")[Rails.env]
-
-HIPCHAT_NOTIFICATIONS_ENABLED  = settings['enabled']
-HIPCHAT_NOTIFICATIONS_SETTINGS = settings['notifications']
