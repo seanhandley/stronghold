3f2fd0c51df72672d52143d9827f22794012ba82
Sean Handley <sean.handley@gmail.com>
2016-01-18 11:33:37 +0000

    Fix tests.

--- test/functional/billing/instances_test.rb
+++ test/functional/billing/instances_test.rb
@@ -14,22 +14,18 @@ class InstancesTest < Minitest::Test
   end
 
   def test_inactive_machines_dont_incur_charges
-    VCR.use_cassette('check_inactives') do
-      assert_equal 3, Billing::Instances.usage(@test_tenant.uuid, Time.parse('2014-11-21 14:00:00'), Time.parse('2014-11-21 15:00:00')).count
-      # All three instances were terminates before 15:00
-      assert_equal 0, Billing::Instances.usage(@test_tenant.uuid, Time.parse('2014-11-21 15:00:00'), Time.parse('2014-11-21 15:30:00')).count
-    end
+    assert_equal 3, Billing::Instances.usage(@test_tenant.uuid, Time.parse('2014-11-21 14:00:00'), Time.parse('2014-11-21 15:00:00')).count
+    # All three instances were terminates before 15:00
+    assert_equal 0, Billing::Instances.usage(@test_tenant.uuid, Time.parse('2014-11-21 15:00:00'), Time.parse('2014-11-21 15:30:00')).count
   end
 
   def test_machines_on_steady_incur_full_usage
-    VCR.use_cassette('check_steady_ons') do
       usage = Billing::Instances.usage(@test_tenant.uuid, Time.parse('2014-11-21 16:10:00'), Time.parse('2014-11-21 16:15:00'))
       assert_equal 1, usage.count
-      measurement_1 = usage.first[:billable_seconds]
-      usage = Billing::Instances.usage(@test_tenant.uuid, Time.parse('2014-11-21 16:10:00'), Time.parse('2014-11-21 16:15:30'))
-      measurement_2 = usage.first[:billable_seconds]
-      assert_equal 30, measurement_2 - measurement_1
-    end
+      measurement_1 = usage.first[:billable_hours]
+      usage = Billing::Instances.usage(@test_tenant.uuid, Time.parse('2014-11-21 16:10:00'), Time.parse('2014-11-21 17:15:00'))
+      measurement_2 = usage.first[:billable_hours]
+      assert_equal 1, measurement_2 - measurement_1
   end
 
   def teardown
