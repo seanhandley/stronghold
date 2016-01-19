c8d094a2bc07b2c91c7cfc3c9e104779acdd8c29
Sean Handley <sean.handley@gmail.com>
2016-01-19 15:24:11 +0000

    No need to debug.

--- lib/constraints/client_cert_constraint.rb
+++ lib/constraints/client_cert_constraint.rb
@@ -2,8 +2,6 @@ require_relative "./staff_constraint"
 
 class ClientCertConstraint
   def matches?(request)
-    Rails.logger.debug '*****'
-    Rails.logger.debug request.host
     return true  unless Rails.env.production?
     return false unless request.host == 'admin-my.datacentred.io'
     return false unless StaffConstraint.new.matches?(request)
