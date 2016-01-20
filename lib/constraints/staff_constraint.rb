7bfc795b2542c396f39ec08403e3740b421b056a
Sean Handley <sean.handley@gmail.com>
2016-01-20 14:38:34 +0000

    Because we need to redirect.

--- lib/constraints/staff_constraint.rb
+++ lib/constraints/staff_constraint.rb
@@ -1,7 +1,6 @@
 class StaffConstraint
   def matches?(request)
-    return false unless request.session[:user_id]
-    return false if (Time.now.utc - Time.parse(request.session[:created_at].to_s).utc) > SESSION_TIMEOUT.minutes
+    return true unless request.session[:user_id]
     user = User.find request.session[:user_id]
     user && user.staff?
   end
