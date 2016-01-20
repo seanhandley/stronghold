895f6a05343c13c20254261d84cbd96b109dda0e
Sean Handley <sean.handley@gmail.com>
2016-01-20 16:54:00 +0000

    Maybe not.

--- lib/constraints/client_cert_constraint.rb
+++ lib/constraints/client_cert_constraint.rb
@@ -3,7 +3,7 @@ require_relative './staff_constraint'
 class ClientCertConstraint
   def matches?(request)
     return true  unless Rails.env.production?
-    return false unless request.host == 'admin-my.datacentred.io'
+    #return false unless request.host == 'admin-my.datacentred.io'
     return false unless StaffConstraint.new.matches?(request)
     return true
   end
