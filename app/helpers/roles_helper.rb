e440882a22b99707a3f7c06f98e4f83dc5bdb5ff
Sean Handley <sean.handley@gmail.com>
2016-01-28 10:59:29 +0000

    Show who has access to which projects.

--- app/helpers/roles_helper.rb
+++ app/helpers/roles_helper.rb
@@ -3,6 +3,10 @@ module RolesHelper
     user.roles.map(&:name).join(', ')
   end
 
+  def list_of_projects(user)
+    user.tenants.map(&:name).uniq.join(', ')
+  end
+
   def roles_for_select(organization)
     options_for_select(organization.roles.collect{|r| [r.name, r.id]})
   end
