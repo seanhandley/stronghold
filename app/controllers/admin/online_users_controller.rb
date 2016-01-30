627f9e1a5cb2ee539acf727b7c0b96233b1d113d
Sean Handley <sean.handley@gmail.com>
2016-01-30 09:24:40 +0000

    Reverse order.

--- app/controllers/admin/online_users_controller.rb
+++ app/controllers/admin/online_users_controller.rb
@@ -1,5 +1,5 @@
 class Admin::OnlineUsersController < AdminBaseController
   def index
-    @users = User.all.select(&:online_today?).sort{|x,y| x[:timestamp] <=> y[:timestamp]}
+    @users = User.all.select(&:online_today?).sort{|x,y| y[:timestamp] <=> x[:timestamp]}
   end
 end
