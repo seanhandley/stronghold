86154f283228e40c73f57d79b9c45e426cd0fede
Sean Handley <sean.handley@gmail.com>
2016-02-04 08:56:58 +0000

    Handle nil.

--- app/jobs/fill_audit_blanks_job.rb
+++ app/jobs/fill_audit_blanks_job.rb
@@ -5,7 +5,7 @@ class FillAuditBlanksJob < ActiveJob::Base
     Audit.where(organization_id: nil).includes(:user).each do |audit|
       if audit.user
         audit.update_column(:organization_id, audit.user.organization_id)
-      elsif organization_id = audit.audited_changes['organization_id']
+      elsif audit.audited_changes && (organization_id = audit.audited_changes['organization_id'])
         audit.update_column(:organization_id, organization_id)
       end
     end
