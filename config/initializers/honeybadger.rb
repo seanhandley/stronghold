b1f542d2fb7713e3c88a957ca7ad7cc625e49099
Sean Handley <sean.handley@gmail.com>
2016-02-11 07:14:50 +0000

    Be quiet, OpenVAS.
    https://app.honeybadger.io/projects/38536/faults/25673502

--- config/initializers/honeybadger.rb
+++ config/initializers/honeybadger.rb
@@ -5,5 +5,6 @@ Honeybadger.configure do |config|
   config.api_key = '5a3dcd9f'
   config.ignore << Sinatra::NotFound
   config.ignore << CanCan::AccessDenied
+  config.ignore << ActionController::UnknownHttpMethod
   config.development_environments = %w(development test acceptance)
 end
