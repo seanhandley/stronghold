995dba22332b5900e0c04a1ce31be9ff030ca68c
Sean Handley <sean.handley@gmail.com>
2016-02-17 17:18:54 +0000

    Handle arrays or strings.

--- app/jobs/deliverhq_mail_job.rb
+++ app/jobs/deliverhq_mail_job.rb
@@ -4,7 +4,7 @@ class DeliverhqMailJob < ActiveJob::Base
   def perform(invite)
     mail = Mailer.signup(invite.id)
     premailer = Premailer::Rails::CustomizedPremailer.new(mail.body.raw_source)
-    message = Deliverhq::send from: mail.from.join(","), to: mail.to.join(","), subject: mail.subject,
+    message = Deliverhq::send from: [mail.from].flatten.join(","), to: mail.to.join(","), subject: mail.subject,
                               html_body: premailer.to_inline_css, plain_body: premailer.to_plain_text
     invite.update_attributes remote_message_id: message.id
   end
