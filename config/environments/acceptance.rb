e44bbff772e61bf1d7761d2eb1f5cb4308d79aeb
Sean Handley <sean.handley@gmail.com>
2016-03-01 13:08:03 +0000

    Should make acceptance tests more robust.

--- config/environments/acceptance.rb
+++ config/environments/acceptance.rb
@@ -10,7 +10,8 @@ Rails.application.configure do
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
+  config.eager_load = true
+  config.allow_concurrency = false
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
