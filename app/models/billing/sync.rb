99ec117f3e7d490e3ef227a904dc870e7b9789ae
Sean Handley <sean.handley@gmail.com>
2016-03-08 13:07:05 +0000

    Include LBs in the count.

--- app/models/billing/sync.rb
+++ app/models/billing/sync.rb
@@ -8,9 +8,10 @@ module Billing
     has_many :volume_states, dependent: :destroy
     has_many :image_states, dependent: :destroy
     has_many :ips, dependent: :destroy
+    has_many :load_balancers, dependent: :destroy
 
     def summary
-      "Took #{(completed_at - started_at).round} seconds to sync #{instance_states.count} instance states, #{volume_states.count} volume states, #{ips.count} IP allocations, and #{image_states.count} image states."
+      "Took #{(completed_at - started_at).round} seconds to sync #{instance_states.count} instance states, #{volume_states.count} volume states, #{ips.count} IP allocations, #{image_states.count} image states, and #{load_balancers.count} load balancers."
     end
   end
 end
