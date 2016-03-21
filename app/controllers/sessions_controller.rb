0b5c6950aa0e6231c09304445f9880bed8603a34
Sean Handley <sean.handley@gmail.com>
2016-03-21 13:55:14 +0000

    Try to expire cookie.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -66,6 +66,6 @@ class SessionsController < ApplicationController
     response.headers["Cache-Control"] = "no-cache, must-revalidate"
     response.headers["Expires"]       = "Thu, 1 Jan 1970 00:00:00 GMT"
     response.headers["Pragma"]        = "no-cache"
+    response.headers["Set-Cookie"]    = "_stronghold_session=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT"
   end
-
 end
