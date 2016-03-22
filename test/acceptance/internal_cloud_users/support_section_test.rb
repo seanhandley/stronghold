538707400dfaca29a2847779d4c92012f82220ae
Sean Handley <sean.handley@gmail.com>
2016-03-22 10:05:38 +0000

    Test support section has loaded correctly.

---
+++ test/acceptance/internal_cloud_users/support_section_test.rb
@@ -0,0 +1,24 @@
+require_relative '../acceptance_test_helper'
+
+class SupportSectionTests < CapybaraTestCase
+  
+  def test_user_can_create_new_ticket
+    visit(support_tickets_path)
+
+    sleep(10)
+
+    page.has_content?('Create a new ticket')
+    find(:xpath, "//a[@ng-click='ticketDialogShow()']").click
+    sleep(1)
+    select('Support', :from => 'new_ticket_department')
+    sleep(1)
+    fill_in('new_ticket_title', :with => 'Test')
+    fill_in('new_ticket_description', :with => 'Test')
+    # find(:xpath, "//button[@ng-click='ticketDialogSubmit()']").click
+    # visit(support_tickets_path)
+
+    # sleep(10)
+    # save_screenshot('/Users/sean/Desktop/screen.png', :full => true)
+  end
+  
+end
