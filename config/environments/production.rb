7a9ef11819c852d0633951bd62a49a31f99d1fc8
Sean Handley <sean.handley@gmail.com>
2016-03-29 07:52:59 +0100

    Raising delivery errors in production is probably a good idea.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Mailer configuration
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address => 'smtp.deliverhq.com',
