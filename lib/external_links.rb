1a9224e021968485ad0412e3bb14dc962791869e
euggrie <eugeniagrieff@gmail.com>
2016-03-30 15:04:36 +0100

    Added link to Documentation on clearing cookies

---
+++ lib/external_links.rb
@@ -0,0 +1,17 @@
+module ExternalLinks
+  def self.cookie_documentation_path
+    'https://docs.datacentred.io/pages/viewpage.action?pageId=2588675'
+  end
+  def self.terms_of_service_path
+    'http://www.datacentred.co.uk/terms-of-service/'
+  end
+  def self.getting_started_path
+    'https://docs.datacentred.io/x/WQAJ'
+  end
+  def self.documentation_path
+    'http://docs.datacentred.io'
+  end
+  def self.status_path
+    'http://status.datacentred.io'
+  end
+end
