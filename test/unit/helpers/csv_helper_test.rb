19e5a04c140e42d6a0a1f9dc185ab02ee62621f9
Sean Handley <sean.handley@gmail.com>
2016-04-06 15:13:39 +0100

    Fix tests.

--- test/unit/helpers/csv_helper_test.rb
+++ test/unit/helpers/csv_helper_test.rb
@@ -8,8 +8,8 @@ class CsvHelperTest < CleanTest
   def setup
     @model = TestModel.new
     @data = [{:name => 'test', :vcpu_hours => 1, :ram_tb_hours => 1, :openstack_tb_hours => 1,
-              :ceph_tb_hours => 1, :contacts => ['foo@bar.com'], :paying => true, :spend => 100.0}]
-    @expected = "Customer,VCPU hours,RAM TBh,OpenStack Storage TBh,Ceph Storage TBh,Admin Contact,Paying?,Spend (£)\ntest,1.0,1.0,1.0,1.0,foo@bar.com,true,100.0\n"
+              :ceph_tb_hours => 1, :load_balancer_hours => 1, :vpn_connection_hours => 1, :contacts => ['foo@bar.com'], :paying => true, :spend => 100.0}]
+    @expected = "Customer,VCPU hours,RAM TBh,OpenStack Storage TBh,Ceph Storage TBh,Load Balancer Hours,VPN Connection Hours,Admin Contact,Paying?,Spend (£)\ntest,1.0,1.0,1.0,1.0,1.0,1.0,foo@bar.com,true,100.0\n"
   end
 
   def test_build_usage_report_csv
