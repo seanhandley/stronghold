691bbc2f59e5b8b9406b27dcc304f39242d48041
Sean Handley <sean.handley@gmail.com>
2016-04-06 15:00:17 +0100

    Fix tests.

--- app/helpers/csv_helper.rb
+++ app/helpers/csv_helper.rb
@@ -6,7 +6,7 @@ module CsvHelper
       csv << ["Customer", "VCPU hours", "RAM TBh", "OpenStack Storage TBh", "Ceph Storage TBh", "Load Balancer Hours", "VPN Connection Hours", "Admin Contact", "Paying?", "Spend (£)"]
       data.each do |entry|
         csv << [entry[:name], entry[:vcpu_hours].round(1), entry[:ram_tb_hours].round(1), entry[:openstack_tb_hours].round(1),
-                entry[:ceph_tb_hours].round(1), entry[:load_balancer_hours].round(2), entry[:vpn_connection_hours],
+                entry[:ceph_tb_hours].round(1), entry[:load_balancer_hours].round(2), entry[:vpn_connection_hours].round(2),
                 entry[:contacts].join(','), entry[:paying], entry[:spend].round(2)]
       end
     end
