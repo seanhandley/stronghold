3ec9c765b67ca2daac183bcbcd949a5a38803648
Sean Handley <sean.handley@gmail.com>
2016-05-10 13:39:34 +0100

    Code style changes.

--- lib/anti_fraud.rb
+++ lib/anti_fraud.rb
@@ -2,22 +2,16 @@ module AntiFraud
   TEST_CHARGE_AMOUNT = 100
 
   def self.test_charge_succeeds?(organization)
-    # Do a charge and a refund. If either fails, return the error.
-    # return [true, 'success'] or [false, 'error message']
     customer_id = organization.stripe_customer_id
 
-    charge = Stripe::Charge.create(
-      :amount => TEST_CHARGE_AMOUNT,
-      :currency => "gbp",
-      :customer => customer_id,
-      :description => "Test charge for #{customer_id}"
-    )
+    charge = Stripe::Charge.create amount:      TEST_CHARGE_AMOUNT,
+                                   currency:    "gbp",
+                                   customer:    customer_id,
+                                   description: "Test charge for #{customer_id}"
 
-    refund = Stripe::Refund.create(
-      charge: charge.id
-    )
+    refund = Stripe::Refund.create charge: charge.id
 
-    if charge.status == 'succeeded' && refund.status == 'succeeded'
+    if [charge, refund].all?{|e| e.status == 'succeeded'}
       [true, 'Test charge succeeded.']
     else
       [false, charge.status == 'succeeded' ? refund.status : charge.status]
