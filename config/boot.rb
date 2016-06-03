c1d8f51674764d7be3ee536e0b23b4c0fee8702e
Sean Handley <sean.handley@gmail.com>
2016-06-03 11:31:45 +0100

    Load defaults file on boot.

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,16 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+defaults = File.expand_path('../../.defaults', __FILE__)
+if File.exists?(defaults)
+  File.readlines(defaults).each_with_index do |line, n|
+    begin
+      next unless line.strip.length > 0
+      values = line.split("=")
+      ENV[values[0].split(' ')[1].strip] = values[1].strip.gsub!("\"","")
+    rescue StandardError
+      puts "Couldn't parse line #{n+1} of .defaults"
+    end
+  end
+end
