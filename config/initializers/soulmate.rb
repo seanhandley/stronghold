55dd2a24a69231329cb4acfd0c733a700d865349
euggrie <eugeniagrieff@gmail.com>
2016-06-13 16:22:36 +0100

    Don't do this in all envs.

--- config/initializers/soulmate.rb
+++ config/initializers/soulmate.rb
@@ -1,6 +1,10 @@
 SOULMATE_SEARCHABLE_MODELS = ["organization"]
 
-unless Rails.env.production?
-  Rails.application.reload_routes!
-  SoulmateJob.perform_now
+if Rails.env.development?
+  begin
+    Rails.application.reload_routes!
+    SoulmateJob.perform_now
+  rescue StandardError => e
+    puts "Couldn't load Soulmate entries: #{e.to_s}"
+  end
 end
