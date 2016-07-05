b7ae61f3b502bd8748495baedfa71d0d2624b514
Sean Handley <sean.handley@gmail.com>
2016-07-05 13:27:36 +0100

    Include resize and Windows data in CSV.

--- app/helpers/csv_helper.rb
+++ app/helpers/csv_helper.rb
@@ -3,11 +3,12 @@ require 'csv'
 module CsvHelper
   def build_usage_report_csv(data)
     CSV.generate do |csv|
-      csv << ["Customer", "VCPU hours", "RAM TBh", "OpenStack Storage TBh", "Ceph Storage TBh", "Load Balancer Hours", "VPN Connection Hours", "Admin Contact", "Paying?", "Spend (£)"]
+      csv << ["Customer", "VCPU hours", "RAM TBh", "OpenStack Storage TBh", "Ceph Storage TBh", "Load Balancer Hours",
+              "VPN Connection Hours", "Admin Contact", "Paying?", "Spend (£)", "Resizes", "Windows?"]
       data.each do |entry|
         csv << [entry[:name], entry[:vcpu_hours].round(1), entry[:ram_tb_hours].round(1), entry[:openstack_tb_hours].round(1),
                 entry[:ceph_tb_hours].round(1), entry[:load_balancer_hours].round(2), entry[:vpn_connection_hours].round(2),
-                entry[:contacts].join(','), entry[:paying], entry[:spend].round(2)]
+                entry[:contacts].join(','), entry[:paying], entry[:spend].round(2), entry[:resizes]&.join(". "), entry[:windows]]
       end
     end
   end
