5e0f74f7ed2b9fd9316063a11cef688624644b97
Sean Handley <sean.handley@gmail.com>
2016-07-09 20:45:39 +0100

    Transition account state properly.

--- app/jobs/fraud_check_job.rb
+++ app/jobs/fraud_check_job.rb
@@ -6,9 +6,8 @@ class FraudCheckJob < ActiveJob::Base
       fc = FraudCheck.new(customer_signup)
       return unless customer_signup.real_ip
       if fc.suspicious?
-        customer_signup.organization.hard_freeze!
+        customer_signup.organization.transition_to! :frozen
         Mailer.fraud_check_alert(customer_signup, fc).deliver_now
-        Mailer.review_mode_alert(customer_signup.organization).deliver_now
       end
     end
   end
