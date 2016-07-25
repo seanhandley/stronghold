5950cfa1bad7c8a7cd520a4c1c1f6254aa3db4ef
Sean Handley <sean.handley@gmail.com>
2016-07-25 15:01:16 +0100

    Ensure state is updated in the DB for the org.

--- app/models/organization_transition.rb
+++ app/models/organization_transition.rb
@@ -2,4 +2,6 @@ class OrganizationTransition < ActiveRecord::Base
   include Statesman::Adapters::ActiveRecordTransition
 
   belongs_to :organization, inverse_of: :transitions
+
+  after_save -> { organization.update_column(:state, to_state) }, on: :create
 end
