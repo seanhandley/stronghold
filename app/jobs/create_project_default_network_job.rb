e31069d9a250850d0ccc11e4162221a265a67882
Sean Handley <sean.handley@gmail.com>
2016-08-26 20:07:06 +0100

    Because Neutron is broken on DevStack.

--- app/jobs/create_project_default_network_job.rb
+++ app/jobs/create_project_default_network_job.rb
@@ -2,6 +2,7 @@ class CreateProjectDefaultNetworkJob < ActiveJob::Base
   queue_as :default
 
   def perform(project_uuid)
+    return if Rails.env.acceptance?
     ProjectResources.new(project_uuid).create_default_network unless ['test','development'].include?(Rails.env)
   end
 end
