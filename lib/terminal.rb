e61ac0ee45b4d9965b256671175e1d0a17dc9235
Sean Handley <sean.handley@gmail.com>
2016-08-30 14:11:22 +0100

    More sophisticated building of OS commands.

--- lib/terminal.rb
+++ lib/terminal.rb
@@ -1,41 +1,30 @@
+require_relative "./terminal/open_stack_command"
+
 class Terminal
   def initialize(credentials)
     @credentials = DOCKER_ARGS.merge(credentials)
   end
 
   def run_command(command)
-    command = <<-EOS
-      openstack #{command} --os-auth-type token --os-auth-url #{@credentials[:auth_url]}
-      --os-token #{Terminal.get_project_tokens(@credentials[:user])[@credentials[:tenant_name]]}
-      --os-tenant-name #{@credentials[:tenant_name]} --os-username #{@credentials[:user].email}
-    EOS
-    command.strip!
-    command.gsub! "\n", ""
-
-    container = image.run(command)
+    return [true, OpenStackCommand.help] if command.strip == 'help'
+    command = OpenStackCommand.new(command, @credentials)
+    Terminal.logger.info command.to_s
+    container = image.run(command.to_s)
     stdout, stderr = container.tap(&:start).attach(logs: true)
     [stderr.empty?, [stdout,stderr].flatten.join]
   ensure
     container&.delete(:force => true)
   end
 
-  def self.get_project_tokens(user, password=nil)
-    Rails.cache.fetch("os_tokens_user_#{user.id}", expires_in: 4.hours, force: !!password) do
-      Hash[user.organization.projects.map {|project|
-        begin
-          args = OPENSTACK_ARGS.dup
-          args.merge!(:openstack_username      => user.email,
-                      :openstack_project_name  => project.reference,
-                      :openstack_api_key       => password)
-          [project.reference, Fog::Identity.new(args).unscoped_token]
-        rescue Excon::Errors::Unauthorized, NameError => e
-          nil
-        end
-      }.compact]
+  private
+
+  def self.logger
+    if Rails.env.production?
+      ::Logger.new("/var/log/rails/stronghold/terminal_commands.log")
+    else
+      Rails.logger
     end
   end
-
-  private
 
   def image
     Docker::Image.get @credentials[:openstack_client_image]
