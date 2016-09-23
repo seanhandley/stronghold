94369363b820c99cb4d9975a29fce54c24f0e6cb
euggrie <eugeniagrieff@gmail.com>
2016-09-23 15:07:06 +0100

    Reorganise UI to use a sidebar layout..

--- app/helpers/states_helper.rb
+++ app/helpers/states_helper.rb
@@ -5,4 +5,30 @@ module StatesHelper
     states = states.map{|state| [state.underscore.humanize.titleize, state]}.sort{|x,y| x[0] <=> y[0]}
     options_for_select(states)
   end
+
+  def colours_for_states(organization)
+    if ["active", "fresh"].include?(organization.state)
+      state_colour = "label label-success"
+    elsif ["frozen", "dormant", "no_payment_methods"].include?(organization.state)
+      state_colour = "label label-warning"
+    else
+      state_colour = "label label-danger"
+    end
+    state_colour
+  end
+
+  def names_for_satates(organization)
+    if ["active", "fresh"].include?(organization.state)
+      state_name = "Active"
+    elsif ["frozen", "dormant", "no_payment_methods"].include?(organization.state)
+      state_name = "In review"
+    else
+      state_name = "Disabled"
+    end
+    state_name
+  end
+
+  def states_info(organization)
+
+  end
 end
