edd5b2aba3d2909695f48916215fd84f3a62d1e8
Eugenia Grieff <eugeniagrieff@gmail.com>
2016-10-24 10:29:51 +0100

    Fix users tab in Admin Section (#269)
    * create controller for users in admin
    
    * Rename show action for index
    
    * nest users inside customers

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,24 @@
+class Admin::UsersController < AdminBaseController
+
+  before_action :get_user, only: [:destroy]
+  before_action :get_organization
+
+
+  def index
+    @users = @organization.users
+  end
+
+  def destroy
+    ajax_response(@user, :destroy, admin_customer_path(@organization))
+  end
+
+  private
+
+  def get_user
+    @user = User.find params[:id]
+  end
+
+  def get_organization
+    @organization = Organization.find params[:customer_id]
+  end
+end
