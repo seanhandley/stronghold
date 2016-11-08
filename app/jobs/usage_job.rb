083c196857a54e31d79d55cf332938cce0c4924e
Sean Handley <seanhandley@users.noreply.github.com>
2016-11-08 13:24:04 +0000

    Make jobs unique. (#281)

--- app/jobs/usage_job.rb
+++ app/jobs/usage_job.rb
@@ -2,7 +2,6 @@ class UsageJob < ActiveJob::Base
   queue_as :default
 
   def perform
-    return if already_running?
     while(mins_since_sync > Billing::SYNC_INTERVAL_MINUTES) do
       Billing.sync!(Billing::SYNC_INTERVAL_MINUTES)
     end
@@ -13,12 +12,5 @@ class UsageJob < ActiveJob::Base
   def mins_since_sync
     last_sync = Billing::Sync.completed.last
     (Time.now - last_sync.period_to) / 60.0
-  end
-
-  def already_running?
-    usage_job_count = Sidekiq::Workers.new.select do |process_id, thread_id, work|
-      work['payload']['wrapped'] == 'UsageJob'
-    end.count
-    usage_job_count > 1
   end
 end
