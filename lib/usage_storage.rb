718cdcbb617baf62e232ca193d8d573b7a17a6c1
Sean Handley <sean@dixiebook.home>
2016-12-08 09:15:54 +0000

    Don't pass to usage.

--- lib/usage_storage.rb
+++ lib/usage_storage.rb
@@ -1,10 +1,11 @@
 module UsageStorage
   def self.fetch(*args, &blk)
+    force = args.delete(:force)
     usage = Billing::Usage.where(*args).first
     usage = Billing::Usage.create(*args) unless usage
     
     unless usage.updated_at > Time.parse("#{usage.year}-#{usage.month}-01").end_of_month
-      if args[:force]
+      if force
         usage.update_attributes blob: yield, updated_at: Time.now
       end
     end
