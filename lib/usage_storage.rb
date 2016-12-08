a8715b0150e15a40d0f58de6488d4e00806cc433
Sean Handley <sean@dixiebook.home>
2016-12-08 13:55:48 +0000

    Force it if you have to.

--- lib/usage_storage.rb
+++ lib/usage_storage.rb
@@ -7,7 +7,7 @@ module UsageStorage
     usage = Billing::Usage.create(clause) unless usage
     
     unless usage.updated_at > Time.parse("#{usage.year}-#{usage.month}-01").end_of_month
-      if args[:force]
+      if args[:force] || usage.updated_at < 1.month.ago
         usage.update_attributes blob: yield, updated_at: Time.now
       end
     end
