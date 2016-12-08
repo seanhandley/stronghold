d6663e606e9a6c5ee314ad4b1c5e9c916468a2f8
Sean Handley <sean@dixiebook.home>
2016-12-08 09:28:16 +0000

    Better.

--- lib/usage_storage.rb
+++ lib/usage_storage.rb
@@ -2,8 +2,8 @@ module UsageStorage
   def self.fetch(*args, &blk)
     force = args.delete(:force)
     allowed_keys = [:year, :month, :organization_id]
-    usage = Billing::Usage.where(*args.slice(allowed_keys)).first
-    usage = Billing::Usage.create(*args.slice(allowed_keys)) unless usage
+    usage = Billing::Usage.where(*(args.slice(allowed_keys))).first
+    usage = Billing::Usage.create(*(args.slice(allowed_keys))) unless usage
     
     unless usage.updated_at > Time.parse("#{usage.year}-#{usage.month}-01").end_of_month
       if force
