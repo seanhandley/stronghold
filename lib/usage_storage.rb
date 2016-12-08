30cd2c5bfa98d7aea5731118be35ade05343fdf3
Sean Handley <sean@dixiebook.home>
2016-12-08 09:36:16 +0000

    This works.

--- lib/usage_storage.rb
+++ lib/usage_storage.rb
@@ -1,9 +1,10 @@
 module UsageStorage
   def self.fetch(*args, &blk)
+    args = args.first
     allowed_keys = [:year, :month, :organization_id]
     clause = args.slice(allowed_keys)
-    usage = Billing::Usage.where(*clause).first
-    usage = Billing::Usage.create(*clause) unless usage
+    usage = Billing::Usage.where(clause).first
+    usage = Billing::Usage.create(clause) unless usage
     
     unless usage.updated_at > Time.parse("#{usage.year}-#{usage.month}-01").end_of_month
       if args[:force]
