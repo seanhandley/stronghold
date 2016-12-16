cd3b045a1d13999a59e0877527e4554ecda84e04
Sean Handley <seanhandley@users.noreply.github.com>
2016-12-16 12:39:22 +0000

    Because Dalli can't marshal objects. (#291)

--- lib/live_cloud_resources.rb
+++ lib/live_cloud_resources.rb
@@ -10,25 +10,25 @@ module LiveCloudResources
 
   def self.servers(refresh_cache: false)
     Rails.cache.fetch("live_servers_dashboard", expires_in: DEFAULT_CACHE_LIFETIME, force: refresh_cache) do
-      OpenStackConnection.compute.servers.all(all_tenants: true)
+      OpenStackConnection.compute.list_servers_detail(all_tenants: true).body['servers']
     end
   end
 
   def self.volumes(refresh_cache: false)
     Rails.cache.fetch("live_volumes_dashboard", expires_in: DEFAULT_CACHE_LIFETIME, force: refresh_cache) do
-      OpenStackConnection.volume.volumes.all(all_tenants: true)
+      OpenStackConnection.volume.list_volumes_detailed(all_tenants: true).body['volumes']
     end
   end
 
   def self.floating_ips(refresh_cache: false)
     Rails.cache.fetch("live_floating_ips_dashboard", expires_in: DEFAULT_CACHE_LIFETIME, force: refresh_cache) do
-      OpenStackConnection.network.floating_ips.all
+      OpenStackConnection.network.list_floating_ips.body['floatingips']
     end
   end
 
   def self.lb_pools(refresh_cache: false)
     Rails.cache.fetch("live_lb_poools_dashboard", expires_in: DEFAULT_CACHE_LIFETIME, force: refresh_cache) do
-      OpenStackConnection.network.lb_pools.all
+      OpenStackConnection.network.list_lb_pools.body['pools']
     end
   end
 end
