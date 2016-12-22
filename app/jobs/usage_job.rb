a8adb4ed12cc47bf28601396e72de75db280fe87
Sean Handley <seanhandley@users.noreply.github.com>
2016-12-22 12:53:45 +0000

    Stop usage jobs trampling cache refreshes. (#300)

--- app/jobs/usage_job.rb
+++ app/jobs/usage_job.rb
@@ -6,9 +6,12 @@ class UsageJob < ActiveJob::Base
   def perform
     return if $usage_job_mutex.locked?
     $usage_job_mutex.synchronize do
+      count = 0
       while(mins_since_sync > Billing::SYNC_INTERVAL_MINUTES) do
         Billing.sync!(Billing::SYNC_INTERVAL_MINUTES)
+        count += 1
       end
+      UsageCacheRefreshJob.perform_later if count > 0
     end
   end
 
