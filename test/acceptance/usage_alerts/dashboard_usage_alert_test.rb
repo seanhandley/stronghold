2b1a4188631c09ff51298ca4f4ebf4c51ab7be39
euggrie <eugeniagrieff@gmail.com>
2016-12-23 10:19:58 +0000

    Add acceptance tests.

---
+++ test/acceptance/usage_alerts/dashboard_usage_alert_test.rb
@@ -0,0 +1,46 @@
+require_relative '../acceptance_test_helper'
+
+class DashboardUsageAlert < CapybaraTestCase
+
+  def setup
+    login("capybara@test.com", '12345678')
+    
+    within('div#quota-limits') do
+      @alerts_message = ""
+    end
+  end
+
+  def test_no_message_when_under_threshold
+    visit('/')
+    sleep 1
+    within('div#quota-limits') do
+      @alerts_message = ""
+      refute page.has_content?('You are reaching your')
+      save_screenshot('/Users/eugenia/Desktop/screen1.png', :full => true)
+    end
+  end
+
+  def test_message_when_all_over_threshold
+    visit('/')
+    sleep 1
+    within('div#quota-limits') do
+      @alerts_message = "You are reaching your Vcpus, Memory, and Storage quota limit"
+      assert page.has_content?("You are reaching your Vcpus, Memory, and Storage quota limit")
+      save_screenshot('/Users/eugenia/Desktop/screen2.png', :full => true)
+    end
+
+  end
+
+  def test_alert_links_to_support_tickets
+    visit('/')
+    sleep 1
+    within('div#quota-limits') do
+      @alerts_message = "You are reaching your Vcpus, Memory, and Storage quota limit"
+      assert has_link?('alert-link')
+    end
+  end
+
+  def teardown
+    Capybara.reset_sessions!
+  end
+end
