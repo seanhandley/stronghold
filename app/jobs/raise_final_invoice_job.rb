a4fa1a2ccf485676b4a8804c2df116d294baf031
Sean Handley <sean@DixieBook.local>
2017-01-04 14:08:01 +0000

    Helps if you actually use a DateTime.

--- app/jobs/raise_final_invoice_job.rb
+++ app/jobs/raise_final_invoice_job.rb
@@ -3,8 +3,8 @@ class RaiseFinalInvoiceJob < ActiveJob::Base
 
   def perform(organization)
     killed_at = organization.transitions.where(to_state: 'closed').first
-    ud = UsageDecorator.new(organization, killed_at.year, killed_at.month)
+    ud = UsageDecorator.new(organization, killed_at.created_at.year, killed_at.created_at.month)
     ud.usage_data(force: true)
-    BillingRunOrgJob.perform_later(organization, killed_at.year, killed_at.month)
+    BillingRunOrgJob.perform_later(organization, killed_at.created_at.year, killed_at.created_at.month)
   end
 end
