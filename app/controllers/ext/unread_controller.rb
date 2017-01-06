62302edeef74fec5d261b12b00d2fd3fc8f91b9b
Sean Handley <sean@dixiebook.home>
2017-01-06 16:54:30 +0000

    Extract info from data.

--- app/controllers/ext/unread_controller.rb
+++ app/controllers/ext/unread_controller.rb
@@ -2,7 +2,11 @@ module Ext
   class UnreadController < ActionController::Base
     def create
       Rails.logger.info '*' * 10
-      Rails.logger.info params.inspect
+      ticket       = params.to_h['ticket']
+      update       = params.to_h['update']
+      organization = Organization.find_by reference: ticket['reference'].split[0]
+      from_user    = User.find_by email: update['from_address']
+      Rails.logger.info "#{organization.name}: #{from_user.name}"
       Rails.logger.info '*' * 10
     end
   end
