5ec24f1d30ffbba40fb9c137e870f3c15e17ebd4
Sean Handley <sean@dixiebook.home>
2017-01-06 20:19:41 +0000

    Strong params it.

--- app/controllers/ext/unread_controller.rb
+++ app/controllers/ext/unread_controller.rb
@@ -2,12 +2,18 @@ module Ext
   class UnreadController < ActionController::Base
     def create
       Rails.logger.info '*' * 10
-      ticket       = params.to_h['ticket']
-      update       = params.to_h['update']
+      ticket       = create_params[:ticket]
+      update       = create_params[:update]
       organization = Organization.find_by reference: ticket['reference'].split[0]
       from_user    = User.find_by email: update['from_address']
       Rails.logger.info "#{organization.name}: #{from_user.name}"
       Rails.logger.info '*' * 10
+    end
+
+    private
+
+    def create_params
+      params.permit(:ticket => ['reference'], :update => ['from_address'])
     end
   end
 end
