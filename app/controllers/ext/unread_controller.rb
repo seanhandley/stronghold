ee1d754d9bbd1d0d055a604acbc8a01fb74337db
Sean Handley <sean@dixiebook.home>
2017-01-10 11:06:29 +0000

    Parse JSON data.

--- app/controllers/ext/unread_controller.rb
+++ app/controllers/ext/unread_controller.rb
@@ -2,8 +2,8 @@ module Ext
   class UnreadController < ActionController::Base
     def create
       Rails.logger.info '*' * 10
-      ticket       = create_params[:ticket]
-      update       = create_params[:update]
+      ticket       = JSON.parse create_params[:ticket]
+      update       = JSON.parse create_params[:update]
       organization = Organization.find_by reference: ticket['reference'].split[0]
       from_user    = User.find_by email: update['from_address']
       Rails.logger.info "#{organization.name}: #{from_user.name}"
@@ -13,7 +13,7 @@ module Ext
     private
 
     def create_params
-      params.permit(:ticket => ['reference'], :update => ['from_address'])
+      params.permit(:ticket, :update)
     end
   end
 end
