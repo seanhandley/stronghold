1054be25e85f4330c23a1a5b78758c4db9c9d4fb
Sean Handley <sean@dixiebook.home>
2017-01-10 11:10:37 +0000

    Wrong keys.

--- app/controllers/ext/unread_controller.rb
+++ app/controllers/ext/unread_controller.rb
@@ -4,7 +4,7 @@ module Ext
       Rails.logger.info '*' * 10
       ticket       = JSON.parse create_params[:ticket]
       update       = JSON.parse create_params[:update]
-      organization = Organization.find_by reference: ticket['reference'].split[0]
+      organization = Organization.find_by reference: ticket['contact']['reference'].split[0]
       from_user    = User.find_by email: update['from_address']
       Rails.logger.info "#{organization.name}: #{from_user.name}"
       Rails.logger.info '*' * 10
