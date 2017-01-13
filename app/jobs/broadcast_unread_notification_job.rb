35d519cb75c624f6aa2733c9ed8f07e71f7c633c
Sean Handley <sean@dixiebook.home>
2017-01-13 17:01:07 +0000

    Not all authors have references.

--- app/jobs/broadcast_unread_notification_job.rb
+++ app/jobs/broadcast_unread_notification_job.rb
@@ -3,7 +3,7 @@ class BroadcastUnreadNotificationJob < ApplicationJob
     from_user    = User.find_by email: update['from_address']
     organization = Organization.find_by reporting_code: ticket['contact']['reference'].split[0]
     updates      = SIRPORTLY.request("ticket_updates/all", ticket: ticket['reference'])
-    users        = updates.map{|u| u['author']['reference'].split[2].gsub(')','').to_i}.uniq.map{|id| User.find_by_id(id)}.compact
+    users        = updates.map{|u| p u; u['from_address']}.uniq.map{|email| User.find_by_email(email)}.compact
     users.reject!{|u| u.id == from_user.id}
     users.reject!{|u| u.staff? } unless organization.staff?
     users.each do |user|
