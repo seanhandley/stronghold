6f7c23cb17007167797046ef18e9c7e848b246f3
Sean Handley <sean@dixiebook.home>
2017-01-13 15:32:18 +0000

    Only notify involved users.

--- app/controllers/ext/unread_controller.rb
+++ app/controllers/ext/unread_controller.rb
@@ -2,12 +2,10 @@ module Ext
   class UnreadController < ActionController::Base
     def create
       ticket       = JSON.parse create_params[:ticket]
-      update       = JSON.parse create_params[:update]
-      organization = Organization.find_by reporting_code: ticket['contact']['reference'].split[0]
-      Rails.logger.info '*' * 10
-      Rails.logger.info ticket["additional_recipients"]
+      update       = JSON.parse create_params[:update]      
       from_user    = User.find_by email: update['from_address']
-      users        = organization.users.reject{|u| u.id == from_user.id}
+      updates      = SIRPORTLY.request("ticket_updates/all", ticket: ticket['reference'])
+      users        = updates.map{|u| u['author']['reference'].split[2].gsub(')','').to_i}.uniq.map{|id| User.find_by_id(id)}.compact
       users.each do |user|
         UnreadTicket.create ticket_id: ticket['reference'],
                             update_id: update['id'],
