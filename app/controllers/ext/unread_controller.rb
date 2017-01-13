a54e316e9086dce8dcd5a6724610543cffac5125
Sean Handley <sean@dixiebook.home>
2017-01-13 16:11:20 +0000

    Separate out logic into a background job.

--- app/controllers/ext/unread_controller.rb
+++ app/controllers/ext/unread_controller.rb
@@ -3,17 +3,7 @@ module Ext
     def create
       ticket       = JSON.parse create_params[:ticket]
       update       = JSON.parse create_params[:update]      
-      from_user    = User.find_by email: update['from_address']
-      organization = Organization.find_by reporting_code: ticket['contact']['reference'].split[0]
-      updates      = SIRPORTLY.request("ticket_updates/all", ticket: ticket['reference'])
-      users        = updates.map{|u| u['author']['reference'].split[2].gsub(')','').to_i}.uniq.map{|id| User.find_by_id(id)}.compact
-      users.reject!{|u| u.id == from_user.id}
-      users.reject!{|u| u.staff? } unless organization.staff?
-      users.each do |user|
-        UnreadTicket.create ticket_id: ticket['reference'],
-                            update_id: update['id'],
-                            user_id:   user.id
-      end
+      BroadcastUnreadNotificationJob.perform_later(ticket, update)
       head :no_content
     end
 
