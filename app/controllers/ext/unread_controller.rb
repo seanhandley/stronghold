5c926a88782a6a7996eeb9d32d578fc1d968049d
Sean Handley <sean@dixiebook.home>
2017-01-13 15:48:47 +0000

    Don't notify staff - we use the helpdesk for that.

--- app/controllers/ext/unread_controller.rb
+++ app/controllers/ext/unread_controller.rb
@@ -6,7 +6,7 @@ module Ext
       from_user    = User.find_by email: update['from_address']
       updates      = SIRPORTLY.request("ticket_updates/all", ticket: ticket['reference'])
       users        = updates.map{|u| u['author']['reference'].split[2].gsub(')','').to_i}.uniq.map{|id| User.find_by_id(id)}.compact
-      users.reject!{|u| u.id == from_user.id }
+      users.reject!{|u| u.id == from_user.id || u.staff? }
       users.each do |user|
         UnreadTicket.create ticket_id: ticket['reference'],
                             update_id: update['id'],
