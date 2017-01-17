d64150ac742defe18f69cbf4ba913ee9a61dd027
Sean Handley <sean@dixiebook.home>
2017-01-17 09:35:27 +0000

    Errors.

--- app/jobs/broadcast_unread_notification_job.rb
+++ app/jobs/broadcast_unread_notification_job.rb
@@ -1,11 +1,11 @@
 class BroadcastUnreadNotificationJob < ApplicationJob
   def perform(ticket, update)
     from_user    = User.find_by email: update['from_address']
-    organization = Organization.find_by reporting_code: ticket['contact']['reference']&.split[0]
+    organization = Organization.find_by reporting_code: ticket['contact']['reference'].split[0] if ticket['contact']['reference']
     updates      = SIRPORTLY.request("ticket_updates/all", ticket: ticket['reference'])
     users        = updates.map{|u| p u; u['from_address']}.uniq.map{|email| User.find_by_email(email)}.compact
     users.reject!{|u| u.id == from_user.id} if from_user
-    users.reject!{|u| u.staff? } unless organization&.staff?
+    users.reject!{|u| u.staff? } unless organization && organization.staff?
     users.each do |user|
       UnreadTicket.create ticket_id: ticket['reference'],
                           update_id: update['id'],
