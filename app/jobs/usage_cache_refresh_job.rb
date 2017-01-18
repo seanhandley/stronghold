61148dbe2e8d9a93cfefbe77f54a2c2d86b951ef
Sean Handley <sean@dixiebook.home>
2017-01-18 12:57:08 +0000

    Don't schedule the slow jobs - just get on with them.

--- app/jobs/usage_cache_refresh_job.rb
+++ app/jobs/usage_cache_refresh_job.rb
@@ -12,8 +12,12 @@ class UsageCacheRefreshJob < ApplicationJob
     dispersal_time = 2 * 3600
     spacing = dispersal_time / Organization.active.count
     organizations.each_with_index do |organization, i|
-      x = (spacing * i * 1.1) + 5
-      UsageCacheRefreshJob.set(wait: x.seconds).perform_later(organization)
+      if organization.slow_jobs?
+        UsageCacheRefreshJob.perform_later(organization)
+      else
+        x = (spacing * i * 1.1) + 5
+        UsageCacheRefreshJob.set(wait: x.seconds).perform_later(organization)
+      end
     end
   end
 
