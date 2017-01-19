81d1eb9bc4e01fc04977f3e54d8f44763958d03b
Sean Handley <sean@dixiebook.home>
2017-01-19 18:20:52 +0000

    Turn on Request ID logging.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   # Set to :debug to see everything in the log.
   config.log_level = :info
   config.logger = ::Logger.new("/var/log/rails/stronghold/production.log")
-  config.log_tags = [ :request_id ]
+  config.log_tags = [ :uuid ]
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
