5f72a228c0ab9ab7e624aea527359c53602c3c68
Eugenia Grieff <eugeniagrieff@gmail.com>
2017-01-25 11:07:44 +0000

    After manually running fraud report from customer view, redirect to customer view. (#317)
    * Fetch org and redirect to costumer view.
    
    * Add some spacing to buttons.

--- app/controllers/admin/frozen_customers_controller.rb
+++ app/controllers/admin/frozen_customers_controller.rb
@@ -1,34 +1,35 @@
 module Admin
   class FrozenCustomersController < AdminBaseController
+    before_action :fetch_organization
+
+    def fetch_organization
+      @organization = Organization.find(params[:id])
+    end
 
     def index
     end
 
     def update
-      @organization = Organization.find(params[:id])
-
       if @organization.unfreeze!
         Mailer.review_mode_successful(@organization).deliver_later
-        redirect_to admin_customer_path, notice: "Customer has been unfrozen."
+        redirect_to admin_customer_path(@organization), notice: "Customer has been unfrozen."
       else
-        redirect_to admin_customer_path, alert: "Couldn't unfreeze!"
+        redirect_to admin_customer_path(@organization), alert: "Couldn't unfreeze!"
       end
     end
 
     def mail
-      organization = Organization.find(params[:id])
-      fc = FraudCheck.new(organization.customer_signup)
-      Mailer.fraud_check_alert(organization.customer_signup, fc, current_user.email).deliver_now
-      redirect_to admin_frozen_customers_path, notice: "Email delivered."
+      fc = FraudCheck.new(@organization.customer_signup)
+      Mailer.fraud_check_alert(@organization.customer_signup, fc, current_user.email).deliver_now
+      redirect_to admin_customer_path(@organization), notice: "Email delivered."
     end
 
     def charge
-      organization = Organization.find(params[:id])
-      status, message = AntiFraud.test_charge_succeeds?(organization)
+      status, message = AntiFraud.test_charge_succeeds?(@organization)
       if status
-        redirect_to admin_frozen_customers_path, notice: 'Success. The amount of £1 was charged and refunded to the customer.'
+        redirect_to aadmin_customer_path(@organization), notice: 'Success. The amount of £1 was charged and refunded to the customer.'
       else
-        redirect_to admin_frozen_customers_path, alert: "Failed. Payment provider says: #{message}"
+        redirect_to admin_customer_path(@organization), alert: "Failed. Payment provider says: #{message}"
       end
     end
 
