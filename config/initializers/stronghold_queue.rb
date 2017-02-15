b12cb60bf6ae2254df3dd4939e990882897cb38e
Sean Handley <sean@dixiebook.home>
2017-02-15 15:29:43 +0000

    No password.

--- config/initializers/stronghold_queue.rb
+++ config/initializers/stronghold_queue.rb
@@ -14,8 +14,8 @@ class StrongholdQueue
   def initialize
     redis_settings = YAML.load_file("#{Rails.root}/config/queue.yml")[Rails.env]
     redis_settings.symbolize_keys!
-    redis_password = Rails.application.secrets.redis_password
-    redis_settings.merge! redis_password.blank? ? {} : {password: redis_password}
+    # redis_password = Rails.application.secrets.redis_password
+    # redis_settings.merge! redis_password.blank? ? {} : {password: redis_password}
     @settings = redis_settings
   end
 end
