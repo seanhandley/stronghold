1245c3bbdb618e49efafd8c1cc82143c4e4bc9e2
Sean Handley <sean@dixiebook.home>
2017-02-20 14:54:30 +0000

    Better.

--- app/jobs/deliverhq_mail_job.rb
+++ app/jobs/deliverhq_mail_job.rb
@@ -6,7 +6,6 @@ class DeliverhqMailJob < ApplicationJob
     # premailer = Premailer::Rails::CustomizedPremailer.new(mail.body.raw_source)
     # message = Deliverhq::send from: [mail.from].flatten.join(","), to: mail.to.join(","), subject: mail.subject,
     #                           html_body: premailer.to_inline_css, plain_body: premailer.to_plain_text
-    premailer = Premailer::Rails::CustomizedPremailer.new(mail.body.raw_source)
     message = Deliverhq::send from: [mail.from].flatten.join(","), to: mail.to.join(","), subject: mail.subject,
                               html_body: mail.html_part.body.decoded #, plain_body: premailer.to_plain_text
     invite.update_column :remote_message_id, message.id
