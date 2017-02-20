155a4d7756f3f9ad6649e009267b4da3a71c293d
Sean Handley <sean@dixiebook.home>
2017-02-20 11:02:57 +0000

    Hopefully fix emails.

--- app/lib/deliver_via_api.rb
+++ app/lib/deliver_via_api.rb
@@ -1,31 +1,32 @@
 module ActionMailer
   class MessageDelivery < Delegator
     def deliver_now_by_api
-      Deliverhq::send mail_attributes
+      send_with_style
+      deliver_now
     end
 
     def deliver_later_by_api
-      DeliverMailByApiJob.perform_later mail_attributes
+      send_with_style
+      deliver_later
     end
 
     private
 
-    def mail_attributes
+    def send_with_style
       options = { with_html_string: true, base_url: 'my.datacentred.io' }
-      premailer = Premailer.new(body.raw_source, options)
+      premailer = Premailer.new(html_part.body.decoded, options)
       plain_text = premailer.to_plain_text
       options.merge! css_string: mail_style.html_safe
-      premailer = Premailer.new(body.raw_source, options)
-      {
-        from: [from].flatten.join(","),
-        to: to.join(","),
-        subject: subject,
-        html_body: premailer.to_inline_css, plain_body: plain_text
-      }
+      premailer = Premailer.new(html_part.body.decoded, options)
+      html_body = premailer.to_inline_css
+
+      html_part = html_body
+      text_part = plain_text
     end
 
     def mail_style
       File.read(Rails.root.join('app/assets/stylesheets/email.css'))
     end
+
   end
 end
