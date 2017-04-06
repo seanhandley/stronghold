abd59a6cfe6e75af5140f3420de8a49e504ee377
Sean Handley <sean.handley@datacentred.co.uk>
2017-04-06 17:19:52 +0100

    Don't do this here.

--- db/migrate/20170404131359_migrate_organization_id_data.rb
+++ db/migrate/20170404131359_migrate_organization_id_data.rb
@@ -1,3 +1,3 @@
 class MigrateOrganizationIdData < ActiveRecord::Migration[5.0]
-  User.all.each{|u| u.update_attributes organizations: [Organization.find_by_id(u.organization_id)].compact }
+  #User.all.each{|u| u.update_attributes organizations: [Organization.find_by_id(u.organization_id)].compact }
 end
