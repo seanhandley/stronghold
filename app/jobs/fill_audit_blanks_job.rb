7ea8d1f8c904aa72c4da79069d76efce3bf0aae5
Sean Handley <sean.handley@datacentred.co.uk>
2017-04-07 10:14:38 +0100

    Refer to ID, not organization ID.

--- app/jobs/fill_audit_blanks_job.rb
+++ app/jobs/fill_audit_blanks_job.rb
@@ -4,7 +4,7 @@ class FillAuditBlanksJob < ApplicationJob
   def perform
     Audit.where(organization_id: nil).includes(:user).each do |audit|
       if audit.user
-        audit.update_column(:organization_id, audit.user.primary_organization.organization_id)
+        audit.update_column(:organization_id, audit.user.primary_organization.id)
       elsif audit.audited_changes && (organization_id = audit.audited_changes['organization_id'])
         audit.update_column(:organization_id, organization_id)
       end
