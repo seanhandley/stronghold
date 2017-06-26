41d21558219d40be5e4f2c2ae8ee2a9f19b18a08
Sean Handley <sean.handley@gmail.com>
2017-06-26 16:27:51 +0100

    Correctly find last week.

--- app/jobs/usage_report_job.rb
+++ app/jobs/usage_report_job.rb
@@ -2,8 +2,8 @@ class UsageReportJob < ApplicationJob
   queue_as :slow
 
   def perform
-    from = (Time.zone.now - 1.day).beginning_of_week
-    to = (Time.zone.now - 1.day).end_of_week
+    from = Time.zone.now.last_week.beginning_of_week
+    to = Time.zone.now.last_week.end_of_week
     data = Reports::UsageReport.new(from, to).contents
     Mailer.usage_report(from.to_s, to.to_s, data).deliver_now
   end
