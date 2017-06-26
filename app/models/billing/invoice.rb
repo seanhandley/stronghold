bf66120bce192b2c7ed48f9cc4dbebcb4024f0d1
Eugenia Grieff <megrieff@gmail.com>
2017-06-26 14:45:42 +0100

    Sort invoices by grand_total

--- app/models/billing/invoice.rb
+++ app/models/billing/invoice.rb
@@ -23,9 +23,9 @@ module Billing
 
     validates :organization, :year, :month, presence: true
 
-    scope :reverse_order,  -> { order('created_at DESC') }
-    scope :unfinalized,    -> { reverse_order.where(finalized: false) }
-    scope :finalized,      -> { reverse_order.where(finalized: true)  }
+    scope :cost_reverse_order,  -> { order('grand_total DESC') }
+    scope :unfinalized,    -> { cost_reverse_order.where(finalized: false) }
+    scope :finalized,      -> { cost_reverse_order.where(finalized: true)  }
     scope :self_service,   -> { joins(:organization).where('organizations.self_service = ?', true) }
     scope :manual_invoice, -> { joins(:organization).where('organizations.self_service = ?', false) }
 
