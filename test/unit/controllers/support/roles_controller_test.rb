9f2d264c531ff5a343bf1d6afd2cbfef36ed2258
Sean Handley <sean.handley@gmail.com>
2017-07-27 15:48:08 +0100

    Rewriting controller tests for new syntax.

--- test/unit/controllers/support/roles_controller_test.rb
+++ test/unit/controllers/support/roles_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class Support::RolesControllerTest < ActionController::TestCase
+class Support::RolesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = User.make!
     @organization = @user.primary_organization
@@ -12,7 +12,7 @@ class Support::RolesControllerTest < ActionController::TestCase
   end
 
   test "index sets the necessary variables" do
-    get :index
+    get support_roles_url
     assert assigns(:roles)
     assert assigns(:users)
     assert assigns(:open_invites)
@@ -20,57 +20,57 @@ class Support::RolesControllerTest < ActionController::TestCase
   end
 
   test "can create new role" do
-    post :create, params: { role: {name: 'foo'}}, format: 'js'
+    post support_roles_url, params: { role: {name: 'foo'}}, xhr: true
     assert assigns(:role)
     assert_response 302
     assert @response.body.include? support_roles_path(tab: 'roles')
   end
 
   test "can't create new role with bad params" do
-    post :create, params: { role: {name: ''}}, format: 'js'
+    post support_roles_url, params: { role: {name: ''}}, xhr: true
     assert_response :unprocessable_entity
     assert @response.body.include? "Name is too short"
   end
 
   test "can rename role" do
-    patch :update, params: { id: @role.id, role: {name: 'bar'}}, format: 'js'
+    patch support_role_url(@role), params: { role: {name: 'bar'}}, xhr: true
     assert_response 302
     assert @response.body.include? support_roles_path(tab: 'roles')
   end
 
   test "can't rename role to be blank" do
-    patch :update, params: { id: @role.id, role: {name: ''}}, format: 'js'
+    patch support_role_url(@role), params: { role: {name: ''}}, xhr: true
     assert_response :unprocessable_entity
     assert @response.body.include? "Name is too short"
   end
 
   test "can update existing role permissions" do
-    patch :update, params: { id: @role2.id, role: {name: @role2.name, permissions: ['roles.read']}}, format: 'js'
+    patch support_role_url(@role2), params: { role: {name: @role2.name, permissions: ['roles.read']}}, xhr: true
     refute @response.body.include? support_roles_path(tab: 'roles')
     assert_response :ok
   end
 
   test "can't update existing role permissions with bad data" do
-    patch :update, params: { id: @role2.id, role: {name: @role2.name, permissions: ['grdfdggdf']}}, format: 'js'
+    patch support_role_url(@role2), params: { role: {name: @role2.name, permissions: ['grdfdggdf']}}, xhr: true
     assert_response :unprocessable_entity
   end
 
   test "can't destroy role if someone is assigned" do
     @user.roles << @role2
     @user.save!
-    delete :destroy, params: { id: @role2.id}
+    delete support_role_url(@role2)
     assert_redirected_to support_roles_path(tab: 'roles')
     assert flash[:notice].include? 'users assigned'
   end
 
   test "can't destroy role if it's the admin role" do
-    delete :destroy, params: { id: @role.id}
+    delete support_role_url(@role)
     assert_redirected_to support_roles_path(tab: 'roles')
     assert flash[:notice].include? 'power user'
   end
 
   test "can destroy role if nobody is assigned" do
-    delete :destroy, params: { id: @role2.id}
+    delete support_role_url(@role2)
     assert_redirected_to support_roles_path(tab: 'roles')
     assert flash[:notice].include? 'success'
   end
--- test/unit/controllers/support/users_controller_test.rb
+++ test/unit/controllers/support/users_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class Support::UsersControllerTest < ActionController::TestCase
+class Support::UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = User.make!
     @user2 = User.make!(organizations: [@user.primary_organization])
@@ -11,48 +11,47 @@ class Support::UsersControllerTest < ActionController::TestCase
   end
 
   test "user can view profile" do
-    get :index
+    get support_profile_path
     assert_response :success
     assert assigns(:user)
     assert_template "support/users/profile"
   end
 
   test "user can update own profile" do
-    patch :update, params: { id: @user.id, user: {first_name: 'Foo'}}, format: 'js'
+    patch support_user_path(@user), params: { user: {first_name: 'Foo'}}, xhr: true
     assert_response :success
     assert @response.body.include? "saved"
   end
 
   test "user can't update with short password" do
-    patch :update, params: { id: @user.id, user: {first_name: 'Foo', password: '1'}}, format: 'js'
+    patch support_user_path(@user), params: { user: {first_name: 'Foo', password: '1'}}, xhr: true
     assert_response :unprocessable_entity
     assert @response.body.include? "too short"
   end
 
   test "reauthenticates with password change" do
-    @controller.stub(:reauthenticate, true) do
-      patch :update, params: { id: @user.id, user: {password: '87654321'}}, format: 'js'
-      assert_response :ok
-      assert @response.body.include? "saved"
-    end
+    patch support_user_path(@user), params: { user: {password: '87654321'}}, xhr: true
+    assert_response :ok
+    assert @response.body.include? "saved"
   end
 
   test "user can't delete users without permission" do
-    delete :destroy, params: { id: @user2.id}, format: 'js'
+    @user.update_attributes(roles: [])
+    delete support_user_path(@user2), xhr: true
     assert_response 302
     assert @response.body.include? support_root_path
   end
 
   test "user can delete another user if they have permission" do
     @user.update_attributes(roles: [@admin_role])
-    delete :destroy, params: { id: @user2.id}, format: 'js'
+    delete support_user_path(@user2), xhr: true
     assert_response 302
     assert @response.body.include? support_roles_path
   end
 
   test "user can't delete self" do
     @user.update_attributes(roles: [@admin_role])
-    delete :destroy, params: {id: @user.id}, format: 'js'
+    delete support_user_path(@user), xhr: true
     assert_response :unprocessable_entity
     assert @response.body.include? "remove yourself"
   end
