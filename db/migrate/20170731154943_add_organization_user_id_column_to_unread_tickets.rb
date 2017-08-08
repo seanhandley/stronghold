93bec1062ba338ee5f2fcd3037d8817488424687
Sean Handley <sean.handley@gmail.com>
2017-08-08 11:18:08 +0100

    Do these in a console.

--- db/migrate/20170731154943_add_organization_user_id_column_to_unread_tickets.rb
+++ db/migrate/20170731154943_add_organization_user_id_column_to_unread_tickets.rb
@@ -2,9 +2,9 @@ class AddOrganizationUserIdColumnToUnreadTickets < ActiveRecord::Migration[5.0]
   def change
     add_column :unread_tickets, :organization_user_id, :integer
 
-    UnreadTicket.find_each do |unread_ticket|
-      organization_user = OrganizationUser.find_by(user: unread_ticket.user, organization: unread_ticket&.user&.primary_organization)
-      organization_user ? unread_ticket.update_attributes(organization_user: organization_user) : unread_ticket.destroy
-    end
+    # UnreadTicket.find_each do |unread_ticket|
+    #   organization_user = OrganizationUser.find_by(user: unread_ticket.user, organization: unread_ticket&.user&.primary_organization)
+    #   organization_user ? unread_ticket.update_attributes(organization_user: organization_user) : unread_ticket.destroy
+    # end
   end
 end
